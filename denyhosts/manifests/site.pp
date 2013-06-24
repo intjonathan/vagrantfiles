2fcbf7fa66067bd892d5948268036fe8165fefd3
Nick Chappell <nick@intronic.org>
2013-06-23 23:33:47 -0700

    Included denyhosts class and some hiera lookup debugging code.

--- denyhosts/manifests/site.pp
+++ denyhosts/manifests/site.pp
@@ -13,3 +13,13 @@ node 'denymaster.local' {
 # include puppetdb::master::config
 
 }
+
+node 'denyclient1' {
+  #$message = hiera_array(allowed_hosts)
+  
+  
+  #$print_message = inline_template('<%= [ message.to_a.join("\n") , "\n" ].join %>')
+  #$print_message = inline_template('<%= message.join("\n") %>')
+  #notify {"your allowed hosts are: ${print_message}":}
+  include denyhosts
+}
