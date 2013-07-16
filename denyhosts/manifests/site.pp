0897f29cd28e658189312af01795899671ea6344
Nick Chappell <nick@intronic.org>
2013-07-15 17:53:25 -0700

    Fixed node hostnames so they use the FQDN that the master can actually find them with.

--- denyhosts/manifests/site.pp
+++ denyhosts/manifests/site.pp
@@ -14,7 +14,7 @@ node 'denymaster.local' {
 
 }
 
-node 'denyclient1' {
+node 'denyclient1.local' {
   #grab data from hiera
   #$message = hiera(allowed_hosts)
   
@@ -25,11 +25,13 @@ node 'denyclient1' {
   include denyhosts
 }
 
-node 'denyclient2' {
+node 'denyclient2.local' {
   #grab data from hiera
-  $message = hiera(allowed_hosts)
+  #$message = hiera(allowed_hosts)
   
   #take the values retrieved from hiera and put newlines between them
-  $print_message = inline_template('<%= message.join("\n") %>')
-  notify {"${print_message}":}
+  #$print_message = inline_template('<%= message.join("\n") %>')
+  #notify {"${print_message}":}
+  
+  include denyhosts
 }
