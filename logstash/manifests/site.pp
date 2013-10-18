d4efd287f0aec3c8196972db4ec598595e8a9484
Nick Chappell <nick@intronic.org>
2013-10-18 00:41:02 -0700

    Added ElasticSearch nodes.

--- logstash/manifests/site.pp
+++ logstash/manifests/site.pp
@@ -4,9 +4,10 @@ node default {
    
 }
 
-node 'elasticsearch1', 'elasticsearch2'{
+node 'elasticsearch1', 'elasticsearch2', 'elasticsearch3', 'elasticsearch4' {
 
     class {'elasticsearch':
         cluster_name => 'mycluster01',
         bind_interface => 'eth1',
     }
+}
