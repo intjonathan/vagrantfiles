c29c24e2b5dae57dae806eb03266756298da892e
Nick Chappell <nick@intronic.org>
2013-10-27 21:10:44 -0700

    Addes an snmpv3user definition.

--- snmp/manifests/site.pp
+++ snmp/manifests/site.pp
@@ -2,5 +2,12 @@ node 'snmpclient1.local', 'snmpclient2.local', 'snmpclient3.local' {
   class {'snmp_client':
     agent_ip_addr_range    => '10.0.1.0/24',
     agent_listen_interface => $ipaddress_eth1,
+  }  
+  
+  snmp_client::snmpv3user {'nick':
+    auth_type => 'SHA',
+    auth_secret => 'password',
+    priv_type => 'AES',
+    priv_secret => 'secret00',
   }
 }
