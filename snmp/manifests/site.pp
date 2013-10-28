2f574a7682ebbc84359ef2f34924d2d28e3b936e
Nick Chappell <nick@intronic.org>
2013-10-27 21:10:08 -0700

    Changed listening interface to eth1.

--- snmp/manifests/site.pp
+++ snmp/manifests/site.pp
@@ -1,5 +1,6 @@
 node 'snmpclient1.local', 'snmpclient2.local', 'snmpclient3.local' {
   class {'snmp_client':
-    agent_ip_addr_range => '10.0.1.0/24',
+    agent_ip_addr_range    => '10.0.1.0/24',
+    agent_listen_interface => $ipaddress_eth1,
   }
 }
