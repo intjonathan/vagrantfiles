5f2982956ef0110609bfb9cad36bb2a6ef2ba4c7
Nick Chappell <nick@intronic.org>
2013-10-28 22:25:42 -0700

    added VACM view parameter.

--- snmp/manifests/site.pp
+++ snmp/manifests/site.pp
@@ -2,6 +2,7 @@ node 'snmpclient1.local', 'snmpclient2.local', 'snmpclient3.local' {
   class {'snmp_client':
     agent_ip_addr_range    => '10.0.1.0/24',
     agent_listen_interface => $ipaddress_eth1,
+    vacm_views => ['rouser nick priv'],
   }  
   
   snmp_client::snmpv3user {'nick':
