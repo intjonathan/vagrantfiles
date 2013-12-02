dfe66d42e292f6b68a688e90d0b07edb2bf28bce
Nick Chappell <nick@intronic.org>
2013-12-01 21:58:03 -0800

    Changed the Ubuntu 13.04 agent script calls to call the 13.10 script instead.

--- puppetmaster/manifests/site.pp
+++ puppetmaster/manifests/site.pp
@@ -8,6 +8,19 @@ node 'master.local' {
 }
 
 
-node 'agent1.local' {
+node 'agent1.local', 'agent2.local', 'agent3.local', 'agent4.local'{
+
+  class {'snmp_client':
+    agent_ip_addr_range    => '10.0.1.0/24',
+    agent_listen_interface => $ipaddress_eth1,
+    vacm_views => ['rouser nick priv'],
+  }  
+  
+  snmp_client::snmpv3user {'nick':
+    auth_type => 'SHA',
+    auth_secret => 'password',
+    priv_type => 'AES',
+    priv_secret => 'secret00',
+  }
 
 }
