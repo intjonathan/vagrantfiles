5f6af4cdb45876ec5fd14ba6c5c5d42f3bcd51a0
Nick Chappell <nick@intronic.org>
2013-12-03 14:29:40 -0800

    Added a Serf project.

---
+++ serf/manifests/site.pp
@@ -0,0 +1,17 @@
+node 'puppet.local' {
+
+#  class { 'puppetdb':
+#    listen_address => '0.0.0.0'
+#  }
+  
+# include puppetdb::master::config  
+}
+
+node 'ubuntuserf.local' {
+
+}
+
+
+node 'centosserf.local' {
+
+}
