7f323ee8f9b872bbabd64c0f317c7e6d48b32869
Nick Chappell <nick@intronic.org>
2013-12-04 20:40:47 -0800

    Add class applications to site.pp.

--- serf/manifests/site.pp
+++ serf/manifests/site.pp
@@ -1,17 +1,21 @@
 node 'puppet.local' {
-
-#  class { 'puppetdb':
-#    listen_address => '0.0.0.0'
-#  }
   
-# include puppetdb::master::config  
+  class { 'puppetdb':
+    listen_address => '0.0.0.0'
+  }
+
+  include puppetdb::master::config  
 }
 
 node 'ubuntuserf.local' {
-
+  class {'serf':
+    version => '0.2.1',
+  }
 }
 
 
 node 'centosserf.local' {
-
+  class {'serf':
+    version => '0.2.1',
+  }
 }
