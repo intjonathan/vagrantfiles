70530d55e5dd4083453390422ef282f52796cf33
Nick Chappell <nick@intronic.org>
2013-12-12 22:35:27 -0800

    Install JDK7 on each machine; install PuppetDB on the Puppet master VM.

--- ant/manifests/site.pp
+++ ant/manifests/site.pp
@@ -2,6 +2,12 @@
 
 node 'antmaster.local' {
 
+  class { 'puppetdb':
+    listen_address => '0.0.0.0'
+  }
+  
+  include puppetdb::master::config
+
   class{ 'ant':
       version => '1.9.2',
   }
@@ -13,6 +19,10 @@ node 'antubuntu.local' {
   class{ 'ant':
       version => '1.9.2',
   }
+  
+ package {'openjdk-7-jdk':
+    ensure => installed,
+  }
  
 }
 
@@ -21,7 +31,11 @@ node 'antdebian.local' {
   class{ 'ant':
       version => '1.9.2',
   }
-  
+
+ package {'openjdk-7-jdk':
+    ensure => installed,
+  }
+
 }
 
 node 'antcentos.local' {
