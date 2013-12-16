1b1d74ba79d2b36b0d0e23d1911201b80428b690
Nick Chappell <nick@intronic.org>
2013-12-15 23:20:22 -0800

    Added Scala class implementations.

--- scala/manifests/site.pp
+++ scala/manifests/site.pp
@@ -1,2 +1,34 @@
-node 'centosscala.local', 'ubuntuscala.local' {
+#Node definitions
+node 'scalamaster.local' {
+
+  class { 'puppetdb':
+    listen_address => '0.0.0.0'
+  }
+  
+  include puppetdb::master::config
+  
+  class{ 'ant':
+      version => '1.9.2',
+  }
+
+}
+
+node 'ubuntuscala.local' {
+
+  class{ 'scala':
+      version => '2.10.2',
+  }
+
+ package {'openjdk-7-jdk':
+    ensure => installed,
+  }
+ 
+}
+
+node 'centosscala.local' {
+
+  class{ 'scala':
+      version => '2.10.2',
+  }
+
 }
