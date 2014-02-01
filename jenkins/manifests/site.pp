64f8eb9ea432968eb421d946c865de44d46d5f46
Nick Chappell <nick@intronic.org>
2014-01-31 21:29:47 -0800

    Added a site.pp manifest.

---
+++ jenkins/manifests/site.pp
@@ -0,0 +1,110 @@
+#default node defition
+node default {
+
+  include ssh
+
+}
+
+#puppet master node definition
+node 'jenkinsmaster.local' {
+
+  class { 'puppetdb':
+    listen_address => '0.0.0.0'
+  }
+  
+  include puppetdb::master::config
+ 
+  class { 'rsyslog::server': }
+ 
+}
+
+#Ubuntu Jenkins server
+node 'ubuntujenkins.local' {
+
+  class { 'rsyslog::client':
+      log_remote     => true,
+      remote_type    => 'tcp',
+      log_local      => true,
+      log_auth_local => true,
+      custom_config  => undef,
+      server         => 'jenkinsmaster.local',
+      port           => '514',
+  }
+
+}
+
+
+#CentOS Jenkins server
+node 'centosjenkins.local' {
+
+  class { 'rsyslog::client':
+      log_remote     => true,
+      remote_type    => 'tcp',
+      log_local      => true,
+      log_auth_local => true,
+      custom_config  => undef,
+      server         => 'jenkinsmaster.local',
+      port           => '514',
+  }
+
+}
+
+#Jenkins worker
+node 'worker1.local' {
+
+  class { 'rsyslog::client':
+      log_remote     => true,
+      remote_type    => 'tcp',
+      log_local      => true,
+      log_auth_local => true,
+      custom_config  => undef,
+      server         => 'ubuntujenkins.local',
+      port           => '514',
+  }
+
+}
+
+#Jenkins worker
+node 'worker2.local' {
+
+  class { 'rsyslog::client':
+      log_remote     => true,
+      remote_type    => 'tcp',
+      log_local      => true,
+      log_auth_local => true,
+      custom_config  => undef,
+      server         => 'ubuntujenkins.local',
+      port           => '514',
+  }
+
+}
+
+#Jenkins worker
+node 'worker3.local' {
+
+  class { 'rsyslog::client':
+      log_remote     => true,
+      remote_type    => 'tcp',
+      log_local      => true,
+      log_auth_local => true,
+      custom_config  => undef,
+      server         => 'ubuntujenkins.local',
+      port           => '514',
+  }
+
+}
+
+#Jenkins worker
+node 'worker4.local' {
+
+  class { 'rsyslog::client':
+      log_remote     => true,
+      remote_type    => 'tcp',
+      log_local      => true,
+      log_auth_local => true,
+      custom_config  => undef,
+      server         => 'ubuntujenkins.local',
+      port           => '514',
+  }
+
+}
