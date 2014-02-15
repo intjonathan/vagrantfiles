f7ad073bf1a0b8c811c5ab2d5cfd069de0cf83d8
Nick Chappell <nick@intronic.org>
2014-02-14 20:19:24 -0800

    Added an SELinux project.

---
+++ selinux/manifests/site.pp
@@ -0,0 +1,51 @@
+#default node defition
+node default {
+
+}
+
+#Puppet master node definition
+node 'selinuxmaster.local' {
+
+  class { 'puppetdb':
+    listen_address => '0.0.0.0'
+  }
+  
+  include puppetdb::master::config
+
+  #include ssh
+  
+  #class { 'rsyslog::server': }
+ 
+}
+
+#Ubuntu ElasticSearch node
+node 'selinux1.local' {
+
+#  include ssh
+
+#  class { 'rsyslog::client':
+#    log_remote     => true,
+#    remote_type    => 'tcp',
+#    log_local      => true,
+#    log_auth_local => true,
+#    custom_config  => undef,
+#    server         => 'elasticsearchmaster.local',
+#    port           => '514',
+#  }
+
+}
+
+#Ubuntu ElasticSearch node
+node 'selinux2.local' {
+
+}
+
+#Ubuntu ElasticSearch node
+node 'selinux3.local' {
+
+}
+
+#Ubuntu ElasticSearch node
+node 'selinux4.local' {
+
+}
