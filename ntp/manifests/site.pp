2279ffc51dcf3ba5d604fa9157fc6c88102434dd
Nick Chappell <nick@intronic.org>
2014-02-20 22:43:32 -0800

    Add the NTP class to the Puppet master.

---
+++ ntp/manifests/site.pp
@@ -0,0 +1,116 @@
+#default ntp defition
+node default {
+
+  include ssh
+
+}
+
+#Puppet master definition
+node 'ntpmaster.local' {
+
+  class { 'puppetdb':
+    listen_address => '0.0.0.0'
+  }
+  
+  include puppetdb::master::config
+ 
+  class { 'rsyslog::server': }
+
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', '2.ubuntu.pool.ntp.org', '3.ubuntu.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
+}
+
+#Ubuntu ntp node
+node 'ntp1.local' {
+
+  include ssh
+
+  class { 'rsyslog::client':
+    log_remote     => true,
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    server         => 'ntpmaster.local',
+    port           => '514',
+  }
+
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', '2.ubuntu.pool.ntp.org', '3.ubuntu.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
+}
+
+#Ubuntu ntp node
+node 'ntp2.local' {
+
+  include ssh
+
+  class { 'rsyslog::client':
+    log_remote     => true,
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    server         => 'ntpmaster.local',
+    port           => '514',
+  }
+
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', '2.ubuntu.pool.ntp.org', '3.ubuntu.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
+}
+
+#CentOS ntp node
+node 'ntp3.local' {
+
+  include ssh
+
+  class { 'rsyslog::client':
+    log_remote     => true,
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    server         => 'ntpmaster.local',
+    port           => '514',
+  }
+
+  class { '::ntp':
+    servers  => [ '0.centos.pool.ntp.org', '1.centos.pool.ntp.org', '2.centos.pool.ntp.org', '3.centos.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
+}
+
+#CentOS ntp node
+node 'ntp4.local' {
+
+  include ssh
+
+  class { 'rsyslog::client':
+    log_remote     => true,
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    server         => 'ntpmaster.local',
+    port           => '514',
+  }
+  
+    class { '::ntp':
+    servers  => [ '0.centos.pool.ntp.org', '1.centos.pool.ntp.org', '2.centos.pool.ntp.org', '3.centos.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+}
