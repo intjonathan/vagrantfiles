8df69dc8d64f2180bf56c4158172f6a3c270b228
Nick Chappell <nick@intronic.org>
2014-03-06 23:05:17 -0800

    Added the SSH and NTP class applications.

--- jenkins/manifests/site.pp
+++ jenkins/manifests/site.pp
@@ -15,11 +15,27 @@ node 'jenkinsmaster.local' {
   include puppetdb::master::config
  
   class { 'rsyslog::server': }
- 
+
+  include ssh
+
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', '2.ubuntu.pool.ntp.org', '3.ubuntu.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
 }
 
 #Ubuntu Jenkins server
 node 'ubuntujenkins.local' {
+
+  include ssh
+
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', '2.ubuntu.pool.ntp.org', '3.ubuntu.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
 
   class { 'rsyslog::client':
       log_remote     => true,
@@ -30,8 +46,45 @@ node 'ubuntujenkins.local' {
       server         => 'jenkinsmaster.local',
       port           => '514',
   }
+
+	class { 'oracle_java':
+		type      => 'jdk',
+		arc       => 'x64',
+		version   => '7u51',
+		os        => 'linux',
+	}
+
+  class { '::jenkins::master':
+    version => '1.15',
+  }
+
+	class { '::jenkins':
+	  install_java => false,
+	  configure_firewall => false,
+	  config_hash => {  
+      'JENKINS_LISTEN_ADDRESS' => {'value' => '10.0.1.49s',} 
+    },
+	}
+
+	::jenkins::plugin {
+    "git" : ;
+  }
   
-  include apache
+  ::jenkins::plugin {
+    "ant" : ;
+  }
+
+  ::jenkins::plugin {
+    "gradle" : ;
+  }
+  
+  #Install Postfix locally so that Jenkins can send out emails
+  class { '::postfix::server':
+    inet_interfaces => 'localhost', #Only listen on localhost
+    inet_protocols => 'all', #Use both IPv4 and IPv6
+    mydomain       => 'netx.net',
+  }
+
 }
 
 
@@ -57,7 +110,7 @@ node 'centosjenkins.local' {
   apache::mod { 'ssl': } #Install/enable the SSL module
   
   #VirtualHost for proxying to a Jenkins install that's running locally
-  apache::vhost { 'jenkins.centosjenkins.local':
+  apache::vhost { "jenkins.${fqdn}":
     port            => 80,
     docroot         => '/var/www',
     servername      => "jenkins.${fqdn}",
@@ -90,7 +143,7 @@ node 'centosjenkins.local' {
 	  install_java => false,
 	  configure_firewall => false,
 	  config_hash => {  
-      'JENKINS_LISTEN_ADDRESS' => {'value' => '127.0.0.1',} 
+      'JENKINS_LISTEN_ADDRESS' => {'value' => '10.0.1.49',} 
     },
 	}
 
@@ -113,14 +166,20 @@ node 'worker1.local' {
 
   include ssh
 
+  class { '::ntp':
+    servers  => [ '0.centos.pool.ntp.org', '1.centos.pool.ntp.org', '2.centos.pool.ntp.org', '3.centos.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
   class { 'rsyslog::client':
-      log_remote     => true,
-      remote_type    => 'tcp',
-      log_local      => true,
-      log_auth_local => true,
-      custom_config  => undef,
-      server         => 'jenkinsmaster.local',
-      port           => '514',
+    log_remote     => true,
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    server         => 'jenkinsmaster.local',
+    port           => '514',
   }
 
 	class { 'oracle_java':
@@ -130,27 +189,34 @@ node 'worker1.local' {
 		os        => 'linux',
 	}
 
-  class { 'jenkins::slave':
-    masterurl => 'http://jenkins.centosjenkins.local',
-    executors => 5,
+  class { '::jenkins::slave':
+    masterurl => 'http://ubuntujenkins.local:8080',
+    executors => 8,
     install_java => false,
+    version => '1.15',
   }
 
 }
 
 #Jenkins worker
 node 'worker2.local' {
-
+  
   include ssh
 
+  class { '::ntp':
+    servers  => [ '0.centos.pool.ntp.org', '1.centos.pool.ntp.org', '2.centos.pool.ntp.org', '3.centos.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
   class { 'rsyslog::client':
-      log_remote     => true,
-      remote_type    => 'tcp',
-      log_local      => true,
-      log_auth_local => true,
-      custom_config  => undef,
-      server         => 'jenkinsmaster.local',
-      port           => '514',
+    log_remote     => true,
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    server         => 'jenkinsmaster.local',
+    port           => '514',
   }
 
 	class { 'oracle_java':
@@ -160,10 +226,11 @@ node 'worker2.local' {
 		os        => 'linux',
 	}
 
-  class { 'jenkins::slave':
-    masterurl => 'http://jenkins.centosjenkins.local',
-    executors => 5,
+  class { '::jenkins::slave':
+    masterurl => 'http://ubuntujenkins.local:8080',
+    executors => 8,
     install_java => false,
+    version => '1.15',
   }
 
 }
@@ -171,18 +238,24 @@ node 'worker2.local' {
 #Jenkins worker
 node 'worker3.local' {
 
-  class { 'rsyslog::client':
-      log_remote     => true,
-      remote_type    => 'tcp',
-      log_local      => true,
-      log_auth_local => true,
-      custom_config  => undef,
-      server         => 'jenkinsmaster.local',
-      port           => '514',
+  include ssh
+
+  class { '::ntp':
+    servers  => [ '0.centos.pool.ntp.org', '1.centos.pool.ntp.org', '2.centos.pool.ntp.org', '3.centos.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
   }
 
-  include ssh
-  
+  class { 'rsyslog::client':
+    log_remote     => true,
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    server         => 'jenkinsmaster.local',
+    port           => '514',
+  }
+
 	class { 'oracle_java':
 		type      => 'jdk',
 		arc       => 'x64',
@@ -190,10 +263,11 @@ node 'worker3.local' {
 		os        => 'linux',
 	}
 
-  class { 'jenkins::slave':
-    masterurl => 'http://jenkins.centosjenkins.local',
-    executors => 5,
+  class { '::jenkins::slave':
+    masterurl => 'http://ubuntujenkins.local:8080',
+    executors => 8,
     install_java => false,
+    version => '1.15',
   }
 
 }
@@ -203,14 +277,20 @@ node 'worker4.local' {
 
   include ssh
 
+  class { '::ntp':
+    servers  => [ '0.centos.pool.ntp.org', '1.centos.pool.ntp.org', '2.centos.pool.ntp.org', '3.centos.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
   class { 'rsyslog::client':
-      log_remote     => true,
-      remote_type    => 'tcp',
-      log_local      => true,
-      log_auth_local => true,
-      custom_config  => undef,
-      server         => 'jenkinsmaster.local',
-      port           => '514',
+    log_remote     => true,
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    server         => 'jenkinsmaster.local',
+    port           => '514',
   }
 
 	class { 'oracle_java':
@@ -220,10 +300,11 @@ node 'worker4.local' {
 		os        => 'linux',
 	}
 
-  class { 'jenkins::slave':
-    masterurl => 'http://jenkins.centosjenkins.local',
-    executors => 5,
+  class { '::jenkins::slave':
+    masterurl => 'http://ubuntujenkins.local:8080',
+    executors => 8,
     install_java => false,
+    version => '1.15',
   }
 
 }
