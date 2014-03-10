3f750250a5d14840e4cd5b62e8ff8e00160823c6
Nick Chappell <nick@intronic.org>
2014-03-09 23:25:50 -0700

    Added nagios resources.

--- icinga/manifests/site.pp
+++ icinga/manifests/site.pp
@@ -16,11 +16,37 @@ node 'icingamaster.local' {
  
   class { 'rsyslog::server': }
  
+  include ssh
+
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', '2.ubuntu.pool.ntp.org', '3.ubuntu.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
 }
 
 #Ubuntu Icinga server node
 
 node 'ubuntuicinga.local' {
+
+  include ssh
+
+  class { 'rsyslog::client':
+      log_remote     => true,
+      remote_type    => 'tcp',
+      log_local      => true,
+      log_auth_local => true,
+      custom_config  => undef,
+      server         => 'icingamaster.local',
+      port           => '514',
+  }
+
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', '2.ubuntu.pool.ntp.org', '3.ubuntu.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
 
   class { 'postgresql::server': }
 
@@ -28,25 +54,143 @@ node 'ubuntuicinga.local' {
     user     => 'icingaidoutils',
     password => postgresql_password('icingaidoutils', 'password'),
   }
-  
+
   class { 'icinga::server':
-    server_db_password => 'password'
+    server_db_password => 'password',
+    server_users => ['icingaadmin', 'nick2', 'nick', 'nick3'],
+  }
+  
+  #An Icinga web user:
+  icinga::server::user { 'nick':
+    password => 'password',
   }
 
-  class { 'rsyslog::client':
-      log_remote     => true,
-      remote_type    => 'tcp',
-      log_local      => true,
-      log_auth_local => true,
-      custom_config  => undef,
-      server         => 'icingamaster.local',
-      port           => '514',
+  #Another Icinga web user:
+  icinga::server::user { 'nick2':
+    password => 'password1', 
+  }
+
+  #Another Icinga web user:
+  icinga::server::user { 'nick3':
+    password => 'password2', 
+  }
+
+#nagios_command { 'check_ping':
+#  command_name => 'check_ping',# (namevar) The name of this nagios_command...
+#  ensure       => present,# The basic property that the resource should be...
+#  command_line => '$USER1$/check_ping -H $HOSTADDRESS$ -w $ARG1$ -c $ARG2$ -p 5',# Nagios configuration file...
+#  target       => '/etc/icinga/objects/commands/check_ping.cfg'
+#  # ...plus any applicable metaparameters.
+#}
+
+  nagios_command { 'check_nrpe':
+    command_name => 'check_nrpe',
+    ensure       => present,
+    command_line => '$USER1$/check_nrpe -H $HOSTADDRESS$ -c $ARG1$ -t 20',
+    target       => "/etc/icinga/objects/commands/check_nrpe.cfg",
+    require      => Class['icinga::server'],
+  }
+
+  nagios_host { 'generic-host':
+    ensure                       => present,
+    target                       => '/etc/icinga/objects/templates/generic-host.cfg',
+    notifications_enabled        => '1',
+    event_handler_enabled        => '1',
+    flap_detection_enabled       => '1',
+    failure_prediction_enabled   => '1',
+    process_perf_data            => '1',
+    retain_status_information    => '1',
+    retain_nonstatus_information => '1',
+    check_command                => 'check-host-alive',
+    max_check_attempts           => '10',
+    notification_interval        => '0',
+    notification_period          => '24x7',
+    notification_options         => 'd,u,r',
+    contact_groups               => 'admins',
+    register                     => '0', #Don't actually register this template
+  }
+
+  nagios_service { 'generic-service':
+    ensure                       => present,
+    target                       => '/etc/icinga/objects/templates/generic-service.cfg',
+    active_checks_enabled        => '1', # Active service checks are enabled
+    passive_checks_enabled       => '1', # Passive service checks are enabled/accepted
+    parallelize_check            => '1', # Active service checks should be parallelized (disabling this can lead to major performance problems)
+    obsess_over_service          => '1', # We should obsess over this service (if necessary)
+    check_freshness              => '0', # Default is to NOT check service 'freshness'
+    notifications_enabled        => '1', # Service notifications are enabled
+    event_handler_enabled        => '1', # Service event handler is enabled
+    flap_detection_enabled       => '1', # Flap detection is enabled
+    failure_prediction_enabled   => '1', # Failure prediction is enabled
+    process_perf_data            => '1', # Process performance data
+    retain_status_information    => '1', # Retain status information across program restarts
+    retain_nonstatus_information => '1', # Retain non-status information across program restarts
+    notification_interval        => '0', # Only send notifications on status change by default.
+    is_volatile                  => '0',  
+    check_period                 => '24x7',
+    normal_check_interval        => '5',  
+    retry_check_interval         => '1', 
+    max_check_attempts           => '4',  
+    notification_period          => '24x7',  
+    notification_options         => 'w,u,c,r',
+    contact_groups               => 'admins',
+    register                     => '0', # DONT REGISTER THIS DEFINITION - ITS NOT A REAL SERVICE, JUST A TEMPLATE!
+  }
+
+  nagios_timeperiod { '24x7':
+    timeperiod_name => '24x7',
+    alias           => '24 Hours A Day, 7 Days A Week',
+    target          => '/etc/icinga/objects/timeperiods/24x7.cfg', #The file this timeperiod definition will get created in
+    sunday          => '00:00-24:00',
+    monday          => '00:00-24:00',
+    tuesday         => '00:00-24:00',
+    wednesday       => '00:00-24:00',
+    thursday        => '00:00-24:00',
+    friday          => '00:00-24:00',
+    saturday        => '00:00-24:00',
+  }
+
+  nagios_hostgroup { 'ssh_servers':
+    ensure         => present,
+    target         => '/etc/icinga/objects/hostgroups/ssh_servers.cfg',
+    hostgroup_name => 'ssh_servers',
+    alias          => 'SSH servers',
+    members        => '*',
+  }
+
+  nagios_contact { 'root':
+    ensure                        => present,
+    target                        => '/etc/icinga/objects/contacts/root.cfg',
+    contact_name                  => 'root',
+    service_notification_period   => '24x7',
+    host_notification_period      => '24x7',
+    service_notification_options  => 'w,u,c,r,f',
+    host_notification_options     => 'd,u,r,f',
+    service_notification_commands => 'notify-service-by-email',
+    host_notification_commands    => 'notify-host-by-email',
+    email                         => 'root@localhost',
+  }
+
+  nagios_contactgroup { 'admins':
+    ensure               => present,
+    target               => '/etc/icinga/objects/contactgroups/admins.cfg',
+    contactgroup_name    => 'admins',
+    alias                => 'Admins',
+    members              => 'root',
+  }
+
+  #Collect all @@nagios_host resources from PuppetDB
+  Nagios_host <<||>> {
+    #notify  => File['/etc/icinga/objects/'],
+    require => Class['icinga::server'],
   }
 
 }
 
 node 'centosicinga.local' {
 
+  include ssh
+
   class { 'rsyslog::client':
       log_remote     => true,
       remote_type    => 'tcp',
@@ -56,13 +200,44 @@ node 'centosicinga.local' {
       server         => 'icingamaster.local',
       port           => '514',
   }
+
+  class { '::ntp':
+    servers  => [ '0.centos.pool.ntp.org', '1.centos.pool.ntp.org', '2.centos.pool.ntp.org', '3.centos.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
+  class { 'icinga::client':
+    nrpe_allowed_hosts => ['10.0.1.78'],
+  }
   
-  include icinga::client
+  icinga::client::command { 'blah':}
+
+  icinga::client::command { 'check_users':
+    nrpe_plugin_name => 'check_users',
+    nrpe_plugin_args => '-w 5 -c 10',
+  }
+  
+#  icinga::client::plugin { 'check_omreport_raid':
+#    source_file => 'puppet:///modules/icinga/check_omreport_raid.pl',
+#  }
+
+  icinga::client::command { 'blah2':}
+  
+  @@nagios_host { $::fqdn:
+    address => $::ipaddress_eth1,
+    check_command => 'check_ping!100.0,20%!500.0,60%',
+    use => 'generic-host',
+    hostgroups => ['ssh_servers'],
+    target => "/etc/icinga/objects/hosts/host_${::fqdn}.cfg",
+  }
 
 }
 
 node 'icingaclient1.local' {
 
+  include ssh
+
   class { 'rsyslog::client':
       log_remote     => true,
       remote_type    => 'tcp',
@@ -72,13 +247,44 @@ node 'icingaclient1.local' {
       server         => 'icingamaster.local',
       port           => '514',
   }
+
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', '2.ubuntu.pool.ntp.org', '3.ubuntu.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
+  class { 'icinga::client':
+    nrpe_allowed_hosts => ['10.0.1.78'],
+  }
   
-  include icinga::client
+  icinga::client::command { 'blah':}
+
+  icinga::client::command { 'check_users':
+    nrpe_plugin_name => 'check_users',
+    nrpe_plugin_args => '-w 5 -c 10',
+  }
+  
+#  icinga::client::plugin { 'check_omreport_raid':
+#    source_file => 'puppet:///modules/icinga/check_omreport_raid.pl',
+#  }
+
+  icinga::client::command { 'blah2':}
+  
+  @@nagios_host { $::fqdn:
+    address => $::ipaddress_eth1,
+    check_command => 'check_ping!100.0,20%!500.0,60%',
+    use => 'generic-host',
+    hostgroups => ['ssh_servers'],
+    target => "/etc/icinga/objects/hosts/host_${::fqdn}.cfg",
+  }
 
 }
 
 node 'icingaclient2.local' {
 
+  include ssh
+
   class { 'rsyslog::client':
       log_remote     => true,
       remote_type    => 'tcp',
@@ -89,6 +295,129 @@ node 'icingaclient2.local' {
       port           => '514',
   }
 
-  include icinga::client
+  class { '::ntp':
+    servers  => [ '0.centos.pool.ntp.org', '1.centos.pool.ntp.org', '2.centos.pool.ntp.org', '3.centos.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+  
+  class { 'icinga::client':
+    nrpe_allowed_hosts => ['10.0.1.78'],
+  }
+
+  icinga::client::command { 'blah':}
+
+  icinga::client::command { 'check_users':
+    nrpe_plugin_name => 'check_users',
+    nrpe_plugin_args => '-w 5 -c 10',
+  }
+  
+#  icinga::client::plugin { 'check_omreport_raid':
+#    source_file => 'puppet:///modules/icinga/check_omreport_raid.pl',
+#  }
+
+  icinga::client::command { 'blah2':}
+
+  @@nagios_host { $::fqdn:
+    address => $::ipaddress_eth1,
+    check_command => 'check_ping!100.0,20%!500.0,60%',
+    use => 'generic-host',
+    hostgroups => ['ssh_servers'],
+    target => "/etc/icinga/objects/hosts/host_${::fqdn}.cfg",
+  }
+
+}
+
+node 'icingaclient3.local' {
+
+  include ssh
+
+  class { 'rsyslog::client':
+      log_remote     => true,
+      remote_type    => 'tcp',
+      log_local      => true,
+      log_auth_local => true,
+      custom_config  => undef,
+      server         => 'icingamaster.local',
+      port           => '514',
+  }
+
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', '2.ubuntu.pool.ntp.org', '3.ubuntu.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
+  class { 'icinga::client':
+    nrpe_allowed_hosts => ['10.0.1.78'],
+  }
+  
+  icinga::client::command { 'blah':}
+
+  icinga::client::command { 'check_users':
+    nrpe_plugin_name => 'check_users',
+    nrpe_plugin_args => '-w 5 -c 10',
+  }
+  
+#  icinga::client::plugin { 'check_omreport_raid':
+#    source_file => 'puppet:///modules/icinga/check_omreport_raid.pl',
+#  }
+
+  icinga::client::command { 'blah2':}
+
+  @@nagios_host { $::fqdn:
+    address => $::ipaddress_eth1,
+    check_command => 'check_ping!100.0,20%!500.0,60%',
+    use => 'generic-host',
+    hostgroups => ['ssh_servers'],
+    target => "/etc/icinga/objects/hosts/host_${::fqdn}.cfg",
+  }
+
+}
+
+node 'icingaclient4.local' {
+
+  include ssh
+
+  class { 'rsyslog::client':
+      log_remote     => true,
+      remote_type    => 'tcp',
+      log_local      => true,
+      log_auth_local => true,
+      custom_config  => undef,
+      server         => 'icingamaster.local',
+      port           => '514',
+  }
+
+  class { '::ntp':
+    servers  => [ '0.centos.pool.ntp.org', '1.centos.pool.ntp.org', '2.centos.pool.ntp.org', '3.centos.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+  
+  class { 'icinga::client':
+    nrpe_allowed_hosts => ['10.0.1.78'],
+  }
+
+  icinga::client::command { 'blah':}
+
+  icinga::client::command { 'check_users':
+    nrpe_plugin_name => 'check_users',
+    nrpe_plugin_args => '-w 5 -c 10',
+  }
+  
+#  icinga::client::plugin { 'check_omreport_raid':
+#    source_file => 'puppet:///modules/icinga/check_omreport_raid.pl',
+#  }
+
+  icinga::client::command { 'blah2':}
+
+  @@nagios_host { $::fqdn:
+    address => $::ipaddress_eth1,
+    check_command => 'check_ping!100.0,20%!500.0,60%',
+    use => 'generic-host',
+    hostgroups => ['ssh_servers'],
+    target => "/etc/icinga/objects/hosts/host_${::fqdn}.cfg",
+  }
 
 }
