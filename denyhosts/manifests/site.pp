c26f9783024e25babc6dae018324684a670947ac
Nick Chappell <nick@intronic.org>
2014-03-21 20:41:46 -0700

    Apply more classes.

--- denyhosts/manifests/site.pp
+++ denyhosts/manifests/site.pp
@@ -11,26 +11,22 @@ node 'denymaster.local' {
   }
   
   include puppetdb::master::config
- 
-#  class { 'rsyslog::server': }
 
-#  class { '::ntp':
-#    servers  => [ '0.ubuntu.pool.node.org', '1.ubuntu.pool.node.org', '2.ubuntu.pool.node.org', '3.ubuntu.pool.node.org' ],
-#    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
-#    disable_monitor => true,
-#  }
+  include ssh
+ 
+  class { 'rsyslog::server': }
+
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.node.org', '1.ubuntu.pool.node.org', '2.ubuntu.pool.node.org', '3.ubuntu.pool.node.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
 
 }
 
 node 'denyclient1.local' {
-  #grab data from hiera
-  #$message = hiera(allowed_hosts)
   
-  #take the values retrieved from hiera and put newlines between them
-  #$print_message = inline_template('<%= message.join("\n") %>')
-  #notify {"${print_message}":}
-  
-  #include denyhosts
+  include denyhosts
 
   include ssh
 
@@ -54,14 +50,8 @@ node 'denyclient1.local' {
 }
 
 node 'denyclient2.local' {
-  #grab data from hiera
-  #$message = hiera(allowed_hosts)
-  
-  #take the values retrieved from hiera and put newlines between them
-  #$print_message = inline_template('<%= message.join("\n") %>')
-  #notify {"${print_message}":}
-  
-  #include denyhosts
+
+  include denyhosts
 
   include ssh
 
