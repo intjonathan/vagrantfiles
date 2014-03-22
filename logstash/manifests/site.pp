114960c3a4440e71af1edb2c87b8f96cf02ce0b0
Nick Chappell <nick@intronic.org>
2014-03-21 23:19:22 -0700

    Apply the SSH class to each node in the LogStash project.

--- logstash/manifests/site.pp
+++ logstash/manifests/site.pp
@@ -6,22 +6,32 @@ node 'logstashmaster.local' {
     listen_address => '0.0.0.0'
   }
   
-  include puppetdb::master::config 
+  include puppetdb::master::config
+  
+  include ssh
 
 }
 
 node 'logstash.local' {
+  
+  include ssh
 
 }
 
 node 'kibanathree.local' {
 
+  include ssh
+  
 }
 
 node 'elasticsearch1.local', 'elasticsearch2.local', 'elasticsearch3.local', 'elasticsearch4.local' {
   
+  include ssh
+  
 }
 
 node 'rsyslog1.local', 'rsyslog2.local' {
+
+  include ssh
 
 }
