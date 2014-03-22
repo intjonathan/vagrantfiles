8c158312c69cf040d6eaf629ca0be4aba1f81e2b
Nick Chappell <nick@intronic.org>
2014-03-21 19:55:12 -0700

    Added NTP, SSH and rsyslog class applications.

--- denyhosts/manifests/site.pp
+++ denyhosts/manifests/site.pp
@@ -6,11 +6,19 @@ node default {
 #puppet master node definition
 node 'denymaster.local' {
 
-#  class { 'puppetdb':
-#    listen_address => '10.0.1.79'
-#  }
+  class { 'puppetdb':
+    listen_address => '0.0.0.0'
+  }
   
-# include puppetdb::master::config
+  include puppetdb::master::config
+ 
+#  class { 'rsyslog::server': }
+
+#  class { '::ntp':
+#    servers  => [ '0.ubuntu.pool.node.org', '1.ubuntu.pool.node.org', '2.ubuntu.pool.node.org', '3.ubuntu.pool.node.org' ],
+#    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+#    disable_monitor => true,
+#  }
 
 }
 
@@ -22,7 +30,27 @@ node 'denyclient1.local' {
   #$print_message = inline_template('<%= message.join("\n") %>')
   #notify {"${print_message}":}
   
-  include denyhosts
+  #include denyhosts
+
+  include ssh
+
+  class { 'rsyslog::client':
+    log_remote     => true,
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    server         => 'denymaster.local',
+    port           => '514',
+  }
+
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.node.org', '1.ubuntu.pool.node.org', '2.ubuntu.pool.node.org', '3.ubuntu.pool.node.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
+
 }
 
 node 'denyclient2.local' {
@@ -33,5 +61,23 @@ node 'denyclient2.local' {
   #$print_message = inline_template('<%= message.join("\n") %>')
   #notify {"${print_message}":}
   
-  include denyhosts
+  #include denyhosts
+
+  include ssh
+
+  class { 'rsyslog::client':
+    log_remote     => true,
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    server         => 'denymaster.local',
+    port           => '514',
+  }
+
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.node.org', '1.ubuntu.pool.node.org', '2.ubuntu.pool.node.org', '3.ubuntu.pool.node.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
 }
