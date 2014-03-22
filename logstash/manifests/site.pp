d5127b33ba6c2118770024a3b68597e5a4fb2f4a
Nick Chappell <nick@intronic.org>
2014-03-21 22:40:34 -0700

    Remove old class applications; we'll put these back in as we need them.

--- logstash/manifests/site.pp
+++ logstash/manifests/site.pp
@@ -8,79 +8,20 @@ node 'logstash.local' {
   
   include puppetdb::master::config 
 
-  class { 'rsyslog::client':
-    log_remote     => true,
-    remote_type    => 'tcp',
-    log_local      => true,
-    log_auth_local => true,
-    custom_config  => undef,
-    server         => 'logstash.local',
-    port           => '5514',
-  }
-
 }
 
-node 'elasticsearch1.local', 'elasticsearch2.local' {
-  
- package {'openjdk-7-jdk':
-    ensure => installed,
- }
-  
-  class { 'elasticsearch':
-    package_url => 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.7.deb',
-    config => {
-      'node'    => {
-        'name' => $fqdn
-      },
-      'index' => {
-        'number_of_replicas' => '1',
-        'number_of_shards'   => '8'
-      },
-      'network' => {
-        'host' => $ipaddress_eth1
-      },
-      'cluster' => {
-        'name' => 'logstash',
-      }
-    }
-  }
-  
-  class { 'rsyslog::client':
-    log_remote     => true,
-    remote_type    => 'tcp',
-    log_local      => true,
-    log_auth_local => true,
-    custom_config  => undef,
-    server         => 'logstash.local',
-    port           => '5514',
-  }
+node 'logstash.local' {
 
 }
 
 node 'kibanathree.local' {
-  
-    class { 'rsyslog::client':
-    log_remote     => true,
-    remote_type    => 'tcp',
-    log_local      => true,
-    log_auth_local => true,
-    custom_config  => undef,
-    server         => 'logstash.local',
-    port           => '5514',
-  }
 
 }
 
-node 'rsyslog1.local', 'rsyslog2.local' {
+node 'elasticsearch1.local', 'elasticsearch2.local', 'elasticsearch3.local', 'elasticsearch4.local' {
+  
+}
 
-  class { 'rsyslog::client':
-    log_remote     => true,
-    remote_type    => 'tcp',
-    log_local      => true,
-    log_auth_local => true,
-    custom_config  => undef,
-    server         => 'logstash.local',
-    port           => '5514',
-  }
+node 'rsyslog1.local', 'rsyslog2.local' {
 
 }
