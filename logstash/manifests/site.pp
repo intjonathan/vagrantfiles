54563d8ae841b4886b599f43aa08e3fce018fc65
Nick Chappell <nick@intronic.org>
2014-03-23 01:06:28 -0700

    Added rsyslog, logstash and elasticsearch configs.

--- logstash/manifests/site.pp
+++ logstash/manifests/site.pp
@@ -16,6 +16,20 @@ node 'logstashmaster.local' {
     disable_monitor => true,
   }
 
+  class { 'rsyslog::client':
+    log_templates => [
+      { name => 'RFC3164fmt', template => '<%PRI%>%TIMESTAMP% %HOSTNAME% %syslogtag%%msg%',},
+    ],
+    log_remote     => true,
+    server         => 'logstash.local',
+    port           => '5514',
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    preserve_fqdn  => true,
+  }
+
 }
 
 node 'logstash.local' {
@@ -26,6 +40,33 @@ node 'logstash.local' {
     servers  => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', '2.ubuntu.pool.ntp.org', '3.ubuntu.pool.ntp.org' ],
     restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
     disable_monitor => true,
+  }
+  
+  file {'/etc/logstash.conf':
+    ensure => file,
+    mode => '0644',
+    owner => 'root',
+    group => 'root',
+    source => 'puppet:///logstash/configs/logstash.conf',  
+  }
+
+  file {'/etc/blah.conf':
+    ensure => file,
+    mode => '0644',
+    owner => 'root',
+    group => 'root',
+    source => 'puppet:///logstash/blah.conf',  
+  }
+  
+  class { 'logstash':
+    manage_repo => true,
+    repo_version => '1.4',
+  }
+  
+  
+  logstash::configfile { 'input_redis':
+    content => template('logstash/input_redis.erb'),
+    order   => 10
   }
 
 }
@@ -39,10 +80,56 @@ node 'kibanathree.local' {
     restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
     disable_monitor => true,
   }
+
+  class { 'rsyslog::client':
+    log_templates => [
+      { name => 'RFC3164fmt', template => '<%PRI%>%TIMESTAMP% %HOSTNAME% %syslogtag%%msg%',},
+    ],
+    log_remote     => true,
+    server         => 'logstash.local',
+    port           => '5514',
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    preserve_fqdn  => true,
+  }
   
 }
 
-node 'elasticsearch1.local', 'elasticsearch2.local', 'elasticsearch3.local', 'elasticsearch4.local' {
+node 'elasticsearch1.local' {
+  
+  class { 'elasticsearch':
+    java_install => true,
+    package_url => 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.0.1.deb',
+    config => {
+      'node'    => {
+        'name' => $fqdn
+      },
+      'index' => {
+        'number_of_replicas' => '1',
+        'number_of_shards'   => '4'
+      },
+      'network' => {
+        'host' => $ipaddress_eth1
+      },
+      'cluster' => {
+        'name' => 'logstash',
+      }
+    }
+  }
+
+  elasticsearch::plugin{'mobz/elasticsearch-head':
+    module_dir => 'head'
+  }
+
+  elasticsearch::plugin{'karmi/elasticsearch-paramedic':
+    module_dir => 'paramedic'
+  }
+
+  elasticsearch::plugin{'lmenezes/elasticsearch-kopf':
+    module_dir => 'kopf'
+  }
   
   include ssh
 
@@ -50,6 +137,46 @@ node 'elasticsearch1.local', 'elasticsearch2.local', 'elasticsearch3.local', 'el
     servers  => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', '2.ubuntu.pool.ntp.org', '3.ubuntu.pool.ntp.org' ],
     restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
     disable_monitor => true,
+  }
+
+  class { 'rsyslog::client':
+    log_templates => [
+      { name => 'RFC3164fmt', template => '<%PRI%>%TIMESTAMP% %HOSTNAME% %syslogtag%%msg%',},
+    ],
+    log_remote     => true,
+    server         => 'logstash.local',
+    port           => '5514',
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    preserve_fqdn  => true,
+  }
+  
+}
+
+node 'elasticsearch2.local', 'elasticsearch3.local', 'elasticsearch4.local' {
+  
+  include ssh
+
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', '2.ubuntu.pool.ntp.org', '3.ubuntu.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
+  class { 'rsyslog::client':
+    log_templates => [
+      { name => 'RFC3164fmt', template => '<%PRI%>%TIMESTAMP% %HOSTNAME% %syslogtag%%msg%',},
+    ],
+    log_remote     => true,
+    server         => 'logstash.local',
+    port           => '5514',
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    preserve_fqdn  => true,
   }
   
 }
@@ -62,6 +189,20 @@ node 'rsyslog1.local', 'rsyslog2.local' {
     servers  => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', '2.ubuntu.pool.ntp.org', '3.ubuntu.pool.ntp.org' ],
     restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
     disable_monitor => true,
+  }
+
+  class { 'rsyslog::client':
+    log_templates => [
+      { name => 'RFC3164fmt', template => '<%PRI%>%TIMESTAMP% %HOSTNAME% %syslogtag%%msg%',},
+    ],
+    log_remote     => true,
+    server         => 'logstash.local',
+    port           => '5514',
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    preserve_fqdn  => true,
   }
 
 }
