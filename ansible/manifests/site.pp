044c813e1e34faf03e011f91539c65d1a98c1e26
Nick Chappell <nick@intronic.org>
2014-03-23 23:19:46 -0700

    Added an Ansible project.

---
+++ ansible/manifests/site.pp
@@ -0,0 +1,161 @@
+#default node defition
+node default {
+
+  include ssh
+
+}
+
+#Puppet master definition
+node 'saltmaster.local' {
+
+  class { 'puppetdb':
+    listen_address => '0.0.0.0'
+  }
+  
+  include puppetdb::master::config
+ 
+  class { 'rsyslog::server': }
+
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.node.org', '1.ubuntu.pool.node.org', '2.ubuntu.pool.node.org', '3.ubuntu.pool.node.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
+}
+
+#Ubuntu salt node
+node 'minion1.local' {
+
+  include ssh
+
+  class { 'rsyslog::client':
+    log_remote     => true,
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    server         => 'saltmaster.local',
+    port           => '514',
+  }
+
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.node.org', '1.ubuntu.pool.node.org', '2.ubuntu.pool.node.org', '3.ubuntu.pool.node.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
+}
+
+#Ubuntu salt node
+node 'minion2.local' {
+
+  include ssh
+
+  class { 'rsyslog::client':
+    log_remote     => true,
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    server         => 'saltmaster.local',
+    port           => '514',
+  }
+
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.node.org', '1.ubuntu.pool.node.org', '2.ubuntu.pool.node.org', '3.ubuntu.pool.node.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
+}
+
+#CentOS salt node
+node 'minion3.local' {
+
+  include ssh
+
+  class { 'rsyslog::client':
+    log_remote     => true,
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    server         => 'saltmaster.local',
+    port           => '514',
+  }
+
+  class { '::ntp':
+    servers  => [ '0.centos.pool.node.org', '1.centos.pool.node.org', '2.centos.pool.node.org', '3.centos.pool.node.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
+}
+
+#CentOS salt node
+node 'minion4.local' {
+
+  include ssh
+
+  class { 'rsyslog::client':
+    log_remote     => true,
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    server         => 'saltmaster.local',
+    port           => '514',
+  }
+  
+    class { '::ntp':
+    servers  => [ '0.centos.pool.node.org', '1.centos.pool.node.org', '2.centos.pool.node.org', '3.centos.pool.node.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+}
+
+#Ubuntu salt node
+node 'minion5.local' {
+
+  include ssh
+
+  class { 'rsyslog::client':
+    log_remote     => true,
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    server         => 'saltmaster.local',
+    port           => '514',
+  }
+  
+    class { '::ntp':
+    servers  => [ '0.ubuntu.pool.node.org', '1.ubuntu.pool.node.org', '2.ubuntu.pool.node.org', '3.ubuntu.pool.node.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
+}
+
+#CentOS salt node
+node 'minion6.local' {
+
+  include ssh
+
+  class { 'rsyslog::client':
+    log_remote     => true,
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    server         => 'saltmaster.local',
+    port           => '514',
+  }
+  
+    class { '::ntp':
+    servers  => [ '0.centos.pool.node.org', '1.centos.pool.node.org', '2.centos.pool.node.org', '3.centos.pool.node.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+}
