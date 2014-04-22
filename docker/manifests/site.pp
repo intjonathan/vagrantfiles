998f60afbea92079d8269b49bc4e5ffb87b914f8
Nick Chappell <nick@intronic.org>
2014-04-21 21:13:17 -0700

    Updated NTP settings for the Docker Puppet master; added Puppetboard module class application stuff.

--- docker/manifests/site.pp
+++ docker/manifests/site.pp
@@ -5,7 +5,7 @@ node 'dockermaster.local' {
   class { 'puppetdb':
     listen_address => '0.0.0.0'
   }
-  
+
   include puppetdb::master::config
 
   #Apache modules for PuppetBoard:
@@ -17,12 +17,26 @@ node 'dockermaster.local' {
     manage_virtualenv => true,
   }
 
+  #A virtualhost for PuppetBoard
+  class { 'puppetboard::apache::vhost':
+    vhost_name => "puppetboard.${fqdn}",
+    port => 80,
+  }
+
   #This module is from: https://github.com/saz/puppet-ssh
   include ssh
+ 
+  #This module is from: https://github.com/saz/puppet-rsyslog
+  class { 'rsyslog::server':
+    enable_tcp => true,
+    enable_udp => true,
+    port       => '514',
+    server_dir => '/var/log/remote/',
+  }
 
   #This module is: https://github.com/puppetlabs/puppetlabs-ntp
   class { '::ntp':
-    servers  => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', '2.ubuntu.pool.ntp.org', '3.ubuntu.pool.ntp.org' ],
+    servers  => [ '0.ubuntu.pool.node.org', '1.ubuntu.pool.node.org', '2.ubuntu.pool.node.org', '3.ubuntu.pool.node.org' ],
     restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
     disable_monitor => true,
   }
