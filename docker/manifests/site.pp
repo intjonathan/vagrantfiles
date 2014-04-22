3f2462801236a3ee4c3c9ed7426ab003bd53d9b7
Nick Chappell <nick@intronic.org>
2014-04-21 21:26:47 -0700

    Install Apache and some modules on docker2.local.

--- docker/manifests/site.pp
+++ docker/manifests/site.pp
@@ -81,6 +81,14 @@ node 'docker2.local' {
 
   include '::docker'
 
+  #Install Apache and some Apache modules so that we can use it as a proxy front-end for sites
+  #hosted in Docker containers:
+  class{ '::apache':}
+  ::apache::mod { 'ssl': } #Install/enable the SSL module
+  ::apache::mod { 'proxy': } #Install/enable the proxy module
+  ::apache::mod { 'proxy_http': } #Install/enable the HTTP proxy module
+  ::apache::mod { 'rewrite': } #Install/enable the rewrite module
+
 }
 
 #Ubuntu Docker machine
