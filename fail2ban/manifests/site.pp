d89a4da5135ecb15c77bd0091f2ca7f1c6432c18
Nick Chappell <nick@intronic.org>
2014-04-22 21:30:57 -0700

    Added a fail2ban::filter definition on failclient3.local.

--- fail2ban/manifests/site.pp
+++ fail2ban/manifests/site.pp
@@ -228,6 +228,23 @@ node 'failclient3.local' {
     bantime => '3600',
   }
 
+  fail2ban::filter {'sshdtesting':
+    daemon => 'sshd',
+    failregex => '
+      ^%(__prefix_line)s(?:error: PAM: )?[aA]uthentication (?:failure|error) for .* from <HOST>( via \S+)?\s*$
+      ^%(__prefix_line)s(?:error: PAM: )?User not known to the underlying authentication module for .* from <HOST>\s*$
+      ^%(__prefix_line)sFailed \S+ for .*? from <HOST>(?: port \d*)?(?: ssh\d*)?(: (ruser .*|(\S+ ID \S+ \(serial \d+\) CA )?\S+ %(__md5hex)s(, client user ".*", client host >
+      ^%(__prefix_line)sROOT LOGIN REFUSED.* FROM <HOST>\s*$
+      ^%(__prefix_line)s[iI](?:llegal|nvalid) user .* from <HOST>\s*$
+      ^%(__prefix_line)sUser .+ from <HOST> not allowed because not listed in AllowUsers\s*$
+      ^%(__prefix_line)sUser .+ from <HOST> not allowed because listed in DenyUsers\s*$
+      ^%(__prefix_line)sUser .+ from <HOST> not allowed because not in any group\s*$
+      ^%(__prefix_line)srefused connect from \S+ \(<HOST>\)\s*$
+      ^%(__prefix_line)sUser .+ from <HOST> not allowed because a group is listed in DenyGroups\s*$
+      ^%(__prefix_line)sUser .+ from <HOST> not allowed because none of user\'s groups are listed in AllowGroups\s*$    
+    ',
+  }
+
   #This module is from: https://github.com/saz/puppet-ssh
   include ssh
 
