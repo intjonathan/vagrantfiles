8da298d6d67477614f404e0ca0491d4cdee91b80
Nick Chappell <nick@intronic.org>
2014-04-27 13:58:40 -0700

    Apply the NTP module to graphite1.local.

--- graphite/manifests/site.pp
+++ graphite/manifests/site.pp
@@ -67,6 +67,13 @@ node 'graphite1.local' {
     port           => '514',
   }
 
+  #This module is: https://github.com/puppetlabs/puppetlabs-ntp
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', '2.ubuntu.pool.ntp.org', '3.ubuntu.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
   #Install Postgres:
   class { 'postgresql::server': }
 
