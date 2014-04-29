836d9ab8ff60a5beeabe25b78b08cf07f96f3c43
Nick Chappell <nick@intronic.org>
2014-04-28 21:53:40 -0700

    Added a Grafana VM to the new metrics project.

--- metrics/riemann_influxdb_grafana_metrics/manifests/site.pp
+++ metrics/riemann_influxdb_grafana_metrics/manifests/site.pp
@@ -211,3 +211,66 @@ node 'riemann.local' {
   }
 
 }
+
+node 'grafana.local' {
+
+  class { 'fail2ban':
+    log_level => '3',
+  }
+
+  fail2ban::jail { 'ssh':
+    enabled  => 'true',
+    port     => 'ssh',
+    filter   => 'sshd',
+    ignoreip => ['1270.0.0.1', '10.0.1.0/24'],
+    logpath  => '/var/log/audit/audit.log',
+    maxretry => '10',
+    bantime => '3600',
+  }
+
+  fail2ban::filter {'sshdtesting':
+    daemon => 'sshd',
+    failregex => '
+      ^%(__prefix_line)s(?:error: PAM: )?[aA]uthentication (?:failure|error) for .* from <HOST>( via \S+)?\s*$
+      ^%(__prefix_line)s(?:error: PAM: )?User not known to the underlying authentication module for .* from <HOST>\s*$
+      ^%(__prefix_line)sFailed \S+ for .*? from <HOST>(?: port \d*)?(?: ssh\d*)?(: (ruser .*|(\S+ ID \S+ \(serial \d+\) CA )?\S+ %(__md5hex)s(, client user ".*", client host >
+      ^%(__prefix_line)sROOT LOGIN REFUSED.* FROM <HOST>\s*$
+      ^%(__prefix_line)s[iI](?:llegal|nvalid) user .* from <HOST>\s*$
+      ^%(__prefix_line)sUser .+ from <HOST> not allowed because not listed in AllowUsers\s*$
+      ^%(__prefix_line)sUser .+ from <HOST> not allowed because listed in DenyUsers\s*$
+      ^%(__prefix_line)sUser .+ from <HOST> not allowed because not in any group\s*$
+      ^%(__prefix_line)srefused connect from \S+ \(<HOST>\)\s*$
+      ^%(__prefix_line)sUser .+ from <HOST> not allowed because a group is listed in DenyGroups\s*$
+      ^%(__prefix_line)sUser .+ from <HOST> not allowed because none of user\'s groups are listed in AllowGroups\s*$    
+    ',
+  }
+
+  #This module is from: https://github.com/saz/puppet-ssh
+  include ssh
+  
+  #This module is from: https://github.com/saz/puppet-rsyslog
+  class { 'rsyslog::client':
+    log_remote     => true,
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    server         => 'grafanamaster.local',
+    port           => '514',
+  }
+
+  #This module is: https://github.com/puppetlabs/puppetlabs-ntp
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', '2.ubuntu.pool.ntp.org', '3.ubuntu.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
+  #Install Postfix locally so that Fail2Ban can send out emails
+  class { '::postfix::server':
+    inet_interfaces => 'localhost', #Only listen on localhost
+    inet_protocols => 'all', #Use both IPv4 and IPv6
+    mydomain       => 'local',
+  }
+
+}
