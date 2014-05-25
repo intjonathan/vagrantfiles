33d6d9b299af1a00d7437bd413fc2a75de1ea9b9
Nick Chappell <nick@intronic.org>
2014-05-25 14:07:45 -0700

    Added Puppetboard module classes.

--- config_management_tools/puppet/manifests/site.pp
+++ config_management_tools/puppet/manifests/site.pp
@@ -18,6 +18,17 @@ node 'saucymaster.local' {
   class { 'apache': }
   class { 'apache::mod::wsgi': }
 
+  #Configure Puppetboard with this module: https://github.com/nibalizer/puppet-module-puppetboard
+  class { 'puppetboard':
+    manage_virtualenv => true,
+  }
+
+  #A virtualhost for PuppetBoard
+  class { 'puppetboard::apache::vhost':
+    vhost_name => "puppetboard.${fqdn}",
+    port => 80,
+  }
+
 }
 
 #Puppet agents
