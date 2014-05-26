ae16b844b64f1c5b3a7b943d502b464452866283
Nick Chappell <nick@intronic.org>
2014-05-25 18:15:45 -0700

    Added a node definition for the Trusty Puppet master.

--- config_management_tools/puppet/manifests/site.pp
+++ config_management_tools/puppet/manifests/site.pp
@@ -43,4 +43,48 @@ node 'saucymaster.local' {
   }
 }
 
+
+node 'trustymaster.local' {
+
+  #This module is from: https://github.com/puppetlabs/puppetlabs-puppetdb/
+  class { 'puppetdb':
+    listen_address => '0.0.0.0'
+  }
+  
+  class { 'puppetdb::master::config':
+    manage_storeconfigs => 'true',
+    manage_routes => 'true',
+  }
+
+  #Apache modules for PuppetBoard:
+  class { 'apache': }
+  class { 'apache::mod::wsgi': }
+
+  #Configure Puppetboard with this module: https://github.com/nibalizer/puppet-module-puppetboard
+  class { 'puppetboard':
+    manage_virtualenv => true,
+  }
+
+  #A virtualhost for PuppetBoard
+  class { 'puppetboard::apache::vhost':
+    vhost_name => "puppetboard.${fqdn}",
+    port => 80,
+  }
+
+  #This module is from: https://github.com/saz/puppet-rsyslog
+  class { 'rsyslog::server':
+    enable_tcp => true,
+    enable_udp => true,
+    port       => '514',
+    server_dir => '/var/log/remote/',
+  }
+
+  #This module is: https://github.com/puppetlabs/puppetlabs-ntp
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', '2.ubuntu.pool.ntp.org', '3.ubuntu.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+}
+
 #Puppet agents
