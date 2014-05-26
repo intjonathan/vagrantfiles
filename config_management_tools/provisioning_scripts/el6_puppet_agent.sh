2ed4b9e58d3c41236f945c7c31945bb576f2e4a2
Nick Chappell <nick@intronic.org>
2014-05-26 13:44:01 -0700

    Cleaned up tab alignment from the EL6 Puppet master and agent scripts.

--- config_management_tools/provisioning_scripts/el6_puppet_agent.sh
+++ config_management_tools/provisioning_scripts/el6_puppet_agent.sh
@@ -1,58 +1,52 @@
 #! /bin/bash
 echo "Checking to see if the Puppet Labs RHEL/CentOS repo needs to be added..."
-
 if [ ! -f /home/vagrant/repos_added.txt ];
 then    
-
 	sudo rpm -ivh http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-7.noarch.rpm
 	sudo yum check-update
 	#Touch the repos_added file to skip this block the next time around
 	touch /home/vagrant/repos_added.txt
-
 else
 	echo "Skipping repo addition..."
 fi
 
 echo "Checking to see if the Puppet agent package needs to be installed..."
-
 if [ ! -f /home/vagrant/puppet_agent_installed.txt ];
 then
-	
 	sudo yum install puppet -y
 	sudo chkconfig --levels 2345 puppet on
 	sudo /etc/init.d/puppet start
 	sudo service iptables stop
-
   sudo cat > /etc/puppet/puppet.conf <<"EOF"
 [main]
-    # The Puppet log directory.
-    # The default value is '$vardir/log'.
-    logdir = /var/log/puppet
+# The Puppet log directory.
+# The default value is '$vardir/log'.
+logdir = /var/log/puppet
 
-    # Where Puppet PID files are kept.
-    # The default value is '$vardir/run'.
-    rundir = /var/run/puppet
+# Where Puppet PID files are kept.
+# The default value is '$vardir/run'.
+rundir = /var/run/puppet
 
-    # Where SSL certificates are kept.
-    # The default value is '$confdir/ssl'.
-    ssldir = /var/lib/puppet/ssl/
-    
-    server=puppet
-    reports=true
-    pluginsync=true
+# Where SSL certificates are kept.
+# The default value is '$confdir/ssl'.
+ssldir = /var/lib/puppet/ssl/
+
+server=puppet
+reports=true
+pluginsync=true
 
 [agent]
-    # The file in which puppetd stores a list of the classes
-    # associated with the retrieved configuratiion.  Can be loaded in
-    # the separate ``puppet`` executable using the ``--loadclasses``
-    # option.
-    # The default value is '$confdir/classes.txt'.
-    classfile = $vardir/classes.txt
+# The file in which puppetd stores a list of the classes
+# associated with the retrieved configuratiion.  Can be loaded in
+# the separate ``puppet`` executable using the ``--loadclasses``
+# option.
+# The default value is '$confdir/classes.txt'.
+classfile = $vardir/classes.txt
 
-    # Where puppetd caches the local configuration.  An
-    # extension indicating the cache format is added automatically.
-    # The default value is '$confdir/localconfig'.
-    localconfig = $vardir/localconfig
+# Where puppetd caches the local configuration.  An
+# extension indicating the cache format is added automatically.
+# The default value is '$confdir/localconfig'.
+localconfig = $vardir/localconfig
 EOF
 	#Touch the puppet_installed.txt file to skip this block the next time around
 	touch /home/vagrant/puppet_agent_installed.txt
