c625c29d614449b7a0326fbfbf99211ae5f69439
Nick Chappell <nick@intronic.org>
2014-06-28 20:53:08 -0700

    Added separate Puppet node definitions for the Ubuntu Trusty and Precise VMs.

--- monitoring/icinga2/manifests/site.pp
+++ monitoring/icinga2/manifests/site.pp
@@ -87,8 +87,8 @@ node 'icinga2master.local' {
 
 }
 
-#Ubuntu icinga2 server node
-node 'ubuntuicinga2.local' {
+#Ubuntu Trusty Icinga2 server node
+node 'trustyicinga2.local' {
 
   #This module is from: https://github.com/saz/puppet-ssh
   class { 'ssh':
@@ -142,6 +142,76 @@ node 'ubuntuicinga2.local' {
   apt::source { 'icinga2_ubuntu_trusty_snapshots_apt':
     location          => 'http://packages.icinga.org/ubuntu',
     release           => 'icinga-trusty',
+    repos             => 'main',
+    required_packages => 'debian-keyring debian-archive-keyring',
+    key               => '34410682',
+    key_source        => 'http://packages.icinga.org/icinga.key',
+    include_src       => true
+  }
+
+  postgresql::server::db { 'icinga2_data':
+    user     => 'icinga2',
+    password => postgresql_password('icinga2', 'password'),
+  }
+
+}
+
+
+#Ubuntu Precise Icinga2 server node
+node 'preciseicinga2.local' {
+
+  #This module is from: https://github.com/saz/puppet-ssh
+  class { 'ssh':
+    #Export host keys to PuppetDB:
+    storeconfigs_enabled => true,
+    server_options => {
+      #Whether to allow password auth; if set to 'no', only SSH keys can be used:
+      #'PasswordAuthentication' => 'no',
+      #How many authentication attempts to allow before disconnecting:
+      'MaxAuthTries'         => '10',
+      'PermitEmptyPasswords' => 'no', 
+      'PermitRootLogin'      => 'no',
+      'Port'                 => [22],
+      'PubkeyAuthentication' => 'yes',
+      #Whether to be strict about the permissions on a user's .ssh/ folder and public keys:
+      'StrictModes'          => 'yes',
+      'TCPKeepAlive'         => 'yes',
+      #Whether to do reverse DNS lookups of client IP addresses when they connect:
+      'UseDNS'               => 'no',
+    },
+  }
+
+  #This module is from: https://github.com/saz/puppet-rsyslog
+  class { 'rsyslog::client':
+    log_remote     => true,
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    server         => 'icinga2master.local',
+    port           => '514',
+  }
+
+  #This module is: https://github.com/puppetlabs/puppetlabs-ntp
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', '2.ubuntu.pool.ntp.org', '3.ubuntu.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
+  #Install Postgres for use as a database with Icinga 2...
+  class { 'postgresql::server': }
+
+  #...and install MySQL as well:
+  class { '::mysql::server':
+    root_password    => 'horsebatterystaple',
+    override_options => { 'mysqld' => { 'max_connections' => '1024' } }
+  }
+
+  #Add the Icinga 2 snapshots apt repo for Ubuntu Saucy Salamander:
+  apt::source { 'icinga2_ubuntu_precise_snapshots_apt':
+    location          => 'http://packages.icinga.org/ubuntu',
+    release           => 'icinga-precise',
     repos             => 'main',
     required_packages => 'debian-keyring debian-archive-keyring',
     key               => '34410682',
