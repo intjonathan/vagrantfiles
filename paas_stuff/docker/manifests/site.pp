581a299db7721f27a3fd41d64475563d0f46f03d
Nick Chappell <nick@intronic.org>
2014-07-11 22:04:50 -0700

    Added a monitoring VM.

--- paas_stuff/docker/manifests/site.pp
+++ paas_stuff/docker/manifests/site.pp
@@ -154,3 +154,107 @@ node 'docker3.local' {
   ::apache::mod { 'rewrite': } #Install/enable the rewrite module
 
 }
+
+#A VM to monitor Docker services with Icinga 2
+node 'dockermonitor.local' {
+
+  #This module is from: https://github.com/saz/puppet-ssh
+  class { 'ssh':
+    #Export host keys to PuppetDB:
+    storeconfigs_enabled => true,
+    server_options => {
+      #Whether to allow password auth; if set to 'no', only SSH keys can be used:
+      #'PasswordAuthentication' => 'no',
+      #How many authentication attempts to allow before disconnecting:
+      'MaxAuthTries'         => '10',
+      'PermitEmptyPasswords' => 'no', 
+      'PermitRootLogin'      => 'no',
+      'Port'                 => [22],
+      'PubkeyAuthentication' => 'yes',
+      #Whether to be strict about the permissions on a user's .ssh/ folder and public keys:
+      'StrictModes'          => 'yes',
+      'TCPKeepAlive'         => 'yes',
+      #Whether to do reverse DNS lookups of client IP addresses when they connect:
+      'UseDNS'               => 'no',
+    },
+  }
+
+  #This module is from: https://github.com/saz/puppet-rsyslog
+  class { 'rsyslog::client':
+    log_remote     => true,
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    server         => 'dockermaster.local',
+    port           => '5514',
+  }
+
+  #This module is: https://github.com/puppetlabs/puppetlabs-ntp
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', '2.ubuntu.pool.ntp.org', '3.ubuntu.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
+  #Install Postgres for use as a database with Icinga 2...
+  class { 'postgresql::server': }
+
+  #...and install MySQL as well:
+  class { '::mysql::server':
+    root_password    => 'horsebatterystaple',
+    override_options => { 'mysqld' => { 'max_connections' => '1024' } }
+  }
+
+  #Create a Postgres DB for Icinga 2:
+  postgresql::server::db { 'icinga2_data':
+    user     => 'icinga2',
+    password => postgresql_password('icinga2', 'password'),
+    grant => 'all',
+  }
+
+  #Create a Postgres DB for Icinga Web 2:
+  postgresql::server::db { 'icingaweb2_data':
+    user     => 'icingaweb2',
+    owner    => 'icinga2',
+    password => postgresql_password('icingaweb2', 'password'),
+  }
+
+  #Create a MySQL database for Icinga 2:
+  mysql::db { 'icinga2_data':
+    user     => 'icinga2',
+    password => 'password',
+    host     => 'localhost',
+    grant    => ['ALL'],
+  }
+
+  #Create a MySQL database for Icinga Web 2:
+  mysql::db { 'icingaweb2_data':
+    user     => 'icingaweb2',
+    password => 'password',
+    host     => 'localhost',
+    grant    => ['ALL'],
+  }
+
+  #Install Icinga 2:
+  #class { 'icinga2::server': 
+  #  server_db_type => 'pgsql',
+  #}
+
+  #Collect all @@icinga2::objects::host resources from PuppetDB that were exported by other machines:
+  #Icinga2::Objects::Host <<| |>> { }
+
+  #Install Postfix so we can monitor SMTP services and send out email alerts:
+  class { '::postfix::server':
+    inet_interfaces => 'all', #Listen on all interfaces
+    inet_protocols => 'all', #Use both IPv4 and IPv6
+    mydomain       => 'local',
+    mynetworks => '127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128 10.0.0.0/16',
+    extra_main_parameters => {
+      'home_mailbox' => 'Maildir/',
+      'mailbox_command' => '',
+      'disable_dns_lookups' => 'yes' #Don't do DNS lookups for MX records since we're just using /etc/hosts for all host lookups
+    }  
+  }
+
+}
