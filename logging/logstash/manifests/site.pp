5d7eceaff9958d13395a9e056057b3b5808326c3
Nick Chappell <nick@intronic.org>
2014-07-15 19:22:37 -0700

    Update the Logstash package version and install the contrib pacakge as well.

--- logging/logstash/manifests/site.pp
+++ logging/logstash/manifests/site.pp
@@ -121,8 +121,11 @@ node 'logstash.local' {
 #  }
   
   class { 'logstash':
-    manage_repo => true,
-    repo_version => '1.4',
+    java_install => true,
+    java_package => 'openjdk-7-jre-headless',
+    package_url => 'https://download.elasticsearch.org/logstash/logstash/packages/debian/logstash_1.4.2-1-2c0f5a1_all.deb',
+    install_contrib => true,
+    contrib_package_url => 'https://download.elasticsearch.org/logstash/logstash/packages/debian/logstash-contrib_1.4.2-1-efd53ef_all.deb',
   }
 
   logstash::configfile { 'logstash_monolithic':
