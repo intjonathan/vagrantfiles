f316aa4c2aa00d5fa64aa23c9907de488ea013bc
Nick Chappell <nick@intronic.org>
2014-07-31 21:36:08 -0700

    Add usermail.local to the imap_servers hostgroup.

--- monitoring/icinga2/manifests/site.pp
+++ monitoring/icinga2/manifests/site.pp
@@ -1845,7 +1845,7 @@ node 'usermail.local' {
   @@icinga2::object::host { $::fqdn:
     display_name => $::fqdn,
     ipv4_address => $::ipaddress_eth1,
-    groups => ['linux_servers', 'mysql_servers', 'postgres_servers', 'clients', 'smtp_servers', 'ssh_servers', 'http_servers'],
+    groups => ['linux_servers', 'mysql_servers', 'postgres_servers', 'clients', 'smtp_servers', 'ssh_servers', 'http_servers', 'imap_servers'],
     vars => {
       os              => 'linux',
       virtual_machine => 'true',
