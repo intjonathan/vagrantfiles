fdd8a91e0f39aa94f11648e63513ddc67ac4fbb5
Nick Chappell <nick@intronic.org>
2014-09-05 19:55:48 -0700

    Make the Puppet master an rsyslog client instead of a server; make it send logs to the ELK server.

--- networky_services/ldap/manifests/site.pp
+++ networky_services/ldap/manifests/site.pp
@@ -24,11 +24,19 @@ node 'ldapmaster.local' {
   }
  
   #This module is from: https://github.com/saz/puppet-rsyslog
-  class { 'rsyslog::server':
-    enable_tcp => true,
-    enable_udp => true,
-    port       => '5514',
-    server_dir => '/var/log/remote/',
+  class { 'rsyslog::client':
+    #Write out logs in RFC3146 format so that they're more consistent when we send them to
+    #Logstash. Logstash is set up to understand this format of logs in its config:
+    log_templates => [
+      { name => 'RFC3164fmt', template => '<%PRI%>%TIMESTAMP% %HOSTNAME% %syslogtag%%msg%',},
+    ],
+    log_remote     => true,
+    server         => 'ldaplogging.local',
+    port           => '5514',
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
   }
  
   #This module is from: https://github.com/saz/puppet-ssh
