7d2402c06c9a60203cbc8268e8ab707dca5f2e8b
Nick Chappell <nick@intronic.org>
2014-09-07 21:09:36 -0700

    Added a node definition for the Consul ELK VM to the site.pp manifest.

--- hashicorp_tools/consul/manifests/site.pp
+++ hashicorp_tools/consul/manifests/site.pp
@@ -315,3 +315,123 @@ node 'consulagent3.local' {
   class { 'icinga::client': }
 
 }
+
+node 'consullogging.local' {
+
+  #This module is from: https://github.com/saz/puppet-ssh
+  class { 'ssh':
+    #Export host keys to PuppetDB:
+    storeconfigs_enabled => true,
+    server_options => {
+      #Whether to allow password auth; if set to 'no', only SSH keys can be used:
+      #'PasswordAuthentication' => 'no',
+      #How many authentication attempts to allow before disconnecting:
+      'MaxAuthTries'         => '10',
+      'PermitEmptyPasswords' => 'no', 
+      'PermitRootLogin'      => 'no',
+      'Port'                 => [22],
+      'PubkeyAuthentication' => 'yes',
+      #Whether to be strict about the permissions on a user's .ssh/ folder and public keys:
+      'StrictModes'          => 'yes',
+      'TCPKeepAlive'         => 'yes',
+      #Whether to do reverse DNS lookups of client IP addresses when they connect:
+      'UseDNS'               => 'no',
+    },
+  }
+
+  #This module is from: https://github.com/saz/puppet-rsyslog
+  class { 'rsyslog::client':
+    log_remote     => true,
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    server         => 'consulmaster.local',
+    port           => '514',
+  }
+
+  #This module is: https://github.com/puppetlabs/puppetlabs-ntp
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', '2.ubuntu.pool.ntp.org', '3.ubuntu.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
+  #Install Apache so we can do a basic service check against HTTP port 80:
+  class { 'apache': 
+    purge_configs => 'true'
+  }
+
+  #Install Logstash:  
+  class { 'logstash':
+    java_install => true,
+    java_package => 'openjdk-7-jre-headless',
+    package_url => 'https://download.elasticsearch.org/logstash/logstash/packages/debian/logstash_1.4.2-1-2c0f5a1_all.deb',
+    install_contrib => true,
+    contrib_package_url => 'https://download.elasticsearch.org/logstash/logstash/packages/debian/logstash-contrib_1.4.2-1-efd53ef_all.deb',
+  }
+
+  logstash::configfile { 'logstash_monolithic':
+    source => 'puppet:///logstash/configs/logstash.conf',
+    order   => 10
+  }
+
+  #Install Elasticsearch...
+  class { 'elasticsearch':
+    java_install => false,
+    package_url => 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.2.deb',
+    config => { 'cluster.name'             => 'logstash',
+                'network.host'             => $ipaddress_eth1,
+                'index.number_of_replicas' => '1',
+                'index.number_of_shards'   => '4',
+    },
+  }
+
+  #...and some plugins:
+  elasticsearch::instance { $fqdn:
+    config => { 'node.name' => $fqdn }
+  }
+
+  elasticsearch::plugin{'mobz/elasticsearch-head':
+    module_dir => 'head',
+    instances  => $fqdn,
+  }
+
+  elasticsearch::plugin{'karmi/elasticsearch-paramedic':
+    module_dir => 'paramedic',
+    instances  => $fqdn,
+  }
+
+  elasticsearch::plugin{'lmenezes/elasticsearch-kopf':
+    module_dir => 'kopf',
+    instances  => $fqdn,
+  }
+
+  #A non-SSL virtual host for Kibana:
+  ::apache::vhost { 'kibana.icinga2logging.local_non-ssl':
+    port            => 80,
+    docroot         => '/sites/apps/kibana3',
+    servername      => "kibana.${fqdn}",
+    access_log => true,
+    access_log_syslog=> 'syslog:local1',
+    error_log => true,
+    error_log_syslog=> 'syslog:local1',
+    custom_fragment => '
+      #Disable multiviews since they can have unpredictable results
+      <Directory "/sites/apps/kibana3">
+        AllowOverride All
+        Require all granted
+        Options -Multiviews
+      </Directory>
+    ',
+  }
+
+  #Create a folder where the SSL certificate and key will live:
+  file {'/etc/apache2/ssl': 
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '600',
+  }
+
+}
