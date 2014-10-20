895e7f1630ce98ef267d61b06f92cb2be80485e3
Nick Chappell <nick@intronic.org>
2014-10-19 17:39:00 -0700

    Create an HTTP check command to test out this PR:
    https://github.com/Icinga/puppet-icinga2/pull/29

--- monitoring/icinga2/manifests/site.pp
+++ monitoring/icinga2/manifests/site.pp
@@ -251,6 +251,39 @@ node 'trustyicinga2server.local' {
 #     categories       => ['DbCatConfig', 'DbCatState', 'DbCatAcknowledgement', 'DbCatComment', 'DbCatDowntime', 'DbCatEventHandler' ],
 #  }
 
+  #Create an HTTP check command to test out this PR: https://github.com/Icinga/puppet-icinga2/pull/29
+  icinga2::object::checkcommand { 'check_http':
+    target_dir => '/etc/icinga2/objects/checkcommands',
+    command => ['"/check_http"'],
+    arguments     => {'"-H"'             => '"$http_vhost$"',
+      '"-I"'          => '"$http_address$"',
+      '"-u"'          => '"$http_uri$"',
+      '"-p"'          => '"$http_port$"',
+      '"-S"'          => {
+        'set_if' => '"$http_ssl$"'
+      },
+      '"--sni"'       => {
+        'set_if' => '"$http_sni$"'
+      },
+      '"-a"'          => {
+        'value'       => '"$http_auth_pair$"',
+        'description' => '"Username:password on sites with basic authentication"'
+      },
+      '"--no-body"'   => {
+        'set_if' => '"$http_ignore_body$"'
+      },
+      '"-r"' => '"$http_expect_body_regex$"',
+      '"-w"' => '"$http_warn_time$"',
+      '"-c"' => '"$http_critical_time$"',
+      '"-e"' => '"$http_expect$"'
+    },
+    vars => {
+      'vars.http_address' => '"$address$"',
+      'vars.http_ssl'     => 'false',
+      'vars.http_sni'     => 'false'
+    }
+  }
+
   #Create a sysloglogger object:
   icinga2::object::sysloglogger { 'syslog-warning':
     severity => 'warning',
