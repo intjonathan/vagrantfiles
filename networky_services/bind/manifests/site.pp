930cd2535e781fcd1b997452820be55e64175be2
Nick Chappell <nick@intronic.org>
2014-10-26 00:26:07 -0700

    Added classes to the node definition for the DNS monitoring server VM.

--- networky_services/bind/manifests/site.pp
+++ networky_services/bind/manifests/site.pp
@@ -948,4 +948,444 @@ node 'dnslogging.local' {
 
 node 'dnsmonitoring.local' {
 
+  ###############################
+  # SSH installation/setup
+  ###############################
+
+ #This module is from: https://github.com/saz/puppet-ssh
+  class { 'ssh':
+    #Export host keys to PuppetDB:
+    storeconfigs_enabled => true,
+    server_options => {
+      #Whether to allow password auth; if set to 'no', only SSH keys can be used:
+      #'PasswordAuthentication' => 'no',
+      #How many authentication attempts to allow before disconnecting:
+      'MaxAuthTries'         => '10',
+      'PermitEmptyPasswords' => 'no', 
+      'PermitRootLogin'      => 'no',
+      'Port'                 => [22],
+      'PubkeyAuthentication' => 'yes',
+      #Whether to be strict about the permissions on a user's .ssh/ folder and public keys:
+      'StrictModes'          => 'yes',
+      'TCPKeepAlive'         => 'yes',
+      #Whether to do reverse DNS lookups of client IP addresses when they connect:
+      'UseDNS'               => 'no',
+    },
+  }
+
+  ###############################
+  # Postfix installation/setup
+  ###############################
+
+  #Install Postfix so we can send out email alerts:
+  class { '::postfix::server':
+    inet_interfaces => 'all', #Listen on all interfaces
+    inet_protocols => 'all', #Use both IPv4 and IPv6
+    mydomain       => 'local',
+    mynetworks => '127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128 10.0.1.0/24',
+    extra_main_parameters => {
+      'home_mailbox' => 'Maildir/',
+      'mailbox_command' => '',
+      'disable_dns_lookups' => 'yes' #Don't do DNS lookups for MX records since we're just using /etc/hosts for all host lookups
+    }  
+  }
+
+  ###############################
+  # rsyslog installation/setup
+  ###############################
+
+  #This module is from: https://github.com/saz/puppet-rsyslog
+  class { 'rsyslog::client':
+    #Write out logs in RFC3146 format so that they're more consistent when we send them to
+    #Logstash. Logstash is set up to understand this format of logs in its config:
+    log_templates => [
+      { name => 'RFC3164fmt', template => '<%PRI%>%TIMESTAMP% %HOSTNAME% %syslogtag%%msg%',},
+    ],
+    log_remote     => true,
+    server         => 'hekalogging.local',
+    port           => '5514',
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+  }
+
+  #Make rsyslog watch the Apache log files:
+  rsyslog::imfile { 'apache-access':
+    file_name => '/var/log/apache2/access.log',
+    file_tag => 'apache-access',
+    file_facility => 'local7',
+    file_severity => 'info',
+  }
+
+  rsyslog::imfile { 'apache-error':
+    file_name => '/var/log/apache2/error.log',
+    file_tag => 'apache-errors',
+    file_facility => 'local7',
+    file_severity => 'error',
+  }
+
+  ###############################
+  # NTP installation/setup
+  ###############################
+
+  #This module is: https://github.com/puppetlabs/puppetlabs-ntp
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', '2.ubuntu.pool.ntp.org', '3.ubuntu.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
+  ###############################
+  # Icinga 2 installation/setup
+  ###############################
+  
+  #Install Postgres for use as a database with Icinga 2...
+  class { 'postgresql::server': } ->
+
+  #Create a Postgres DB for Icinga 2:
+  postgresql::server::db { 'icinga2_data':
+    user     => 'icinga2',
+    password => postgresql_password('icinga2', 'password'),
+    grant => 'all',
+  } ->
+
+  #Create a Postgres DB for Icinga Web 2:
+  postgresql::server::db { 'icingaweb2_data':
+    user     => 'icingaweb2',
+    password => postgresql_password('icingaweb2', 'password'),
+    grant => 'all',
+  } ->
+
+  #Install Icinga 2:
+  class { 'icinga2::server': 
+    server_db_type => 'pgsql',
+    db_user        => 'icinga2',
+    db_name        => 'icinga2_data',
+    db_password    => 'password',
+    db_host        => '127.0.0.1',
+    db_port        => '5432',
+    server_install_nagios_plugins => false,
+    install_mail_utils_package => true,
+    server_enabled_features  => ['checker','notification', 'livestatus', 'syslog'],
+    server_disabled_features => ['graphite', 'api'],
+  } ->
+
+  #Collect all @@icinga2::object::host resources from PuppetDB that were exported by other machines:
+  Icinga2::Object::Host <<| |>> { }
+
+  #Create a linux_servers hostgroup:
+  icinga2::object::hostgroup { 'linux_servers':
+    display_name => 'Linux servers',
+    groups => ['mysql_servers', 'clients'],
+    target_dir => '/etc/icinga2/objects/hostgroups',
+    assign_where => 'match("*redis*", host.name) || match("*elastic*", host.name) || match("*logstash*", host.name)',
+  }
+
+  #Create a mysql_servers hostgroup:
+  icinga2::object::hostgroup { 'mysql_servers':
+    display_name => 'MySQL servers',
+    target_dir => '/etc/icinga2/objects/hostgroups',
+  }
+
+  #Postgres IDO connection object:
+  icinga2::object::idopgsqlconnection { 'testing_postgres':
+     host             => '127.0.0.1',
+     port             => 5432,
+     user             => 'icinga2',
+     password         => 'password',
+     database         => 'icinga2_data',
+     target_file_name => 'ido-pgsql.conf',
+     target_dir       => '/etc/icinga2/features-enabled',
+     categories       => ['DbCatConfig', 'DbCatState', 'DbCatAcknowledgement', 'DbCatComment', 'DbCatDowntime', 'DbCatEventHandler' ],
+  }
+
+  #Create a sysloglogger object:
+  icinga2::object::sysloglogger { 'syslog-warning':
+    severity => 'warning',
+    target_dir => '/etc/icinga2/features-enabled',
+  }
+
+  #Create a user definition:
+  icinga2::object::user { 'nick':
+    display_name => 'Nick',
+    email => 'nick@usermail.local',
+    period => '24x7',
+    enable_notifications => 'true',
+    groups => [ 'admins' ],
+    states => [ 'OK', 'Warning', 'Critical', 'Unknown', 'Up', 'Down' ],
+    types => [ 'Problem', 'Recovery', 'Acknowledgement', 'Custom', 'DowntimeStart', 'DowntimeEnd', 'DowntimeRemoved', 'FlappingStart', 'FlappingEnd' ],
+    target_dir => '/etc/icinga2/objects/users',
+  }
+
+  #Create an admins user group:
+  icinga2::object::usergroup { 'admins':
+    display_name => 'admins',
+    target_dir => '/etc/icinga2/objects/usergroups',
+  }
+
+  #Create a postgres_servers hostgroup:
+  icinga2::object::hostgroup { 'postgres_servers':
+    display_name => 'Postgres servers',
+    target_dir => '/etc/icinga2/objects/hostgroups',
+  }
+
+  #Create an IMAP servers hostgroup:
+  icinga2::object::hostgroup { 'imap_servers':
+    display_name => 'IMAP servers',
+    target_dir => '/etc/icinga2/objects/hostgroups',
+  }
+
+  #Create an HTTP servers hostgroup:
+  icinga2::object::hostgroup { 'http_servers':
+    display_name => 'HTTP servers',
+    target_dir => '/etc/icinga2/objects/hostgroups',
+  }
+
+  #Create an SSH servers hostgroup:
+  icinga2::object::hostgroup { 'ssh_servers':
+    display_name => 'SSH servers',
+    target_dir => '/etc/icinga2/objects/hostgroups',
+  }
+
+  #Create a clients hostgroup:
+  icinga2::object::hostgroup { 'clients':
+    display_name => 'Client machines',
+    target_dir => '/etc/icinga2/objects/hostgroups',
+  }
+
+  #Create an SMTP servers hostgroup
+  icinga2::object::hostgroup { 'smtp_servers':
+    display_name => 'SMTP servers',
+    target_dir => '/etc/icinga2/objects/hostgroups',
+  }
+
+  #Dependency object to test out this PR: https://github.com/Icinga/puppet-icinga2/pull/28
+  icinga2::object::dependency { "usermail to icinga2mail":
+    object_name => "usermail_dep_on_icinga2mail",
+    parent_host_name => 'icinga2mail.local',
+    child_host_name => 'usermail.local',
+    target_dir => '/etc/icinga2/objects/dependencies',
+    target_file_name => "usermail_to_icinga2mail.conf",
+  }
+
+  #Apply_dependency object to test out this PR: https://github.com/Icinga/puppet-icinga2/pull/28
+  icinga2::object::apply_dependency { 'usermail_dep_on_icinga2mail':
+    parent_host_name => 'icinga2mail.local',
+    assign_where => 'match("^usermail*", host.name)',
+  }
+
+  #Apply_dependency object to test out this PR: https://github.com/Icinga/puppet-icinga2/pull/28
+  icinga2::object::apply_dependency { 'imap_dep_on_smtp':
+    parent_service_name => 'check_ssh',
+    object_type => 'Service',
+    assign_where => 'match("^check_smtp*", service.name)',
+  }
+
+  #Create a web services servicegroup:
+  icinga2::object::servicegroup { 'web_services':
+    display_name => 'web services',
+    target_dir => '/etc/icinga2/objects/servicegroups',
+  }
+
+  #Create a database services servicegroup:
+  icinga2::object::servicegroup { 'database_services':
+    display_name => 'database services',
+    target_dir => '/etc/icinga2/objects/servicegroups',
+  }
+  
+  #Create an email services servicegroup:
+  icinga2::object::servicegroup { 'email_services':
+    display_name => 'email services',
+    target_dir => '/etc/icinga2/objects/servicegroups',
+  }
+
+  #Create a machine health services servicegroup:
+  icinga2::object::servicegroup { 'machine_health':
+    display_name => 'machine health',
+    target_dir => '/etc/icinga2/objects/servicegroups',
+  }
+
+  #Create an apply that checks SSH:
+  icinga2::object::apply_service_to_host { 'check_ssh':
+    display_name => 'SSH',
+    check_command => 'ssh',
+    vars => {
+      service_type => 'production',
+    },
+    assign_where => '"ssh_servers" in host.groups',
+    ignore_where => 'host.name == "localhost"',
+    target_dir => '/etc/icinga2/objects/applys'
+  }
+
+  #Create an apply that checks SMTP:
+  icinga2::object::apply_service_to_host { 'check_smtp':
+    display_name => 'SMTP',
+    check_command => 'smtp',
+    vars => {
+      service_type => 'production',
+    },
+    assign_where => '"linux_servers" in host.groups',
+    ignore_where => 'host.name == "localhost"',
+    target_dir => '/etc/icinga2/objects/applys'
+  }
+
+  #Create an apply that checks load average:
+  icinga2::object::apply_service_to_host { 'load_average':
+    display_name => 'Load average',
+    check_command => 'nrpe',
+    vars => {
+      service_type => 'production',
+      nrpe_command => 'check_load',
+    },
+    assign_where => '"linux_servers" in host.groups',
+    ignore_where => 'host.name == "localhost"',
+    target_dir => '/etc/icinga2/objects/applys'
+  }
+
+  #Create an apply that checks the number of users:
+  icinga2::object::apply_service_to_host { 'check_users':
+    display_name => 'Logged in users',
+    check_command => 'nrpe',
+    vars => {
+      service_type => 'production',
+      nrpe_command => 'check_users',
+    },
+    assign_where => '"linux_servers" in host.groups',
+    ignore_where => 'host.name == "localhost"',
+    target_dir => '/etc/icinga2/objects/applys'
+  }
+
+  #Create an apply that checks disk space on /:
+  icinga2::object::apply_service_to_host { 'check_disk':
+    display_name => 'Disk space on /',
+    check_command => 'nrpe',
+    vars => {
+      service_type => 'production',
+      nrpe_command => 'check_disk',
+    },
+    assign_where => '"linux_servers" in host.groups',
+    ignore_where => 'host.name == "localhost"',
+    target_dir => '/etc/icinga2/objects/applys'
+  }
+
+  #Create an apply that checks the number of total processes:
+  icinga2::object::apply_service_to_host { 'check_total_procs':
+    display_name => 'Total procs',
+    check_command => 'nrpe',
+    vars => {
+      service_type => 'production',
+      nrpe_command => 'check_total_procs',
+    },
+    assign_where => '"linux_servers" in host.groups',
+    ignore_where => 'host.name == "localhost"',
+    target_dir => '/etc/icinga2/objects/applys'
+  }
+
+  #Create an apply that checks the number of zombie processes:
+  icinga2::object::apply_service_to_host { 'check_zombie_procs':
+    display_name => 'Zombie procs',
+    check_command => 'nrpe',
+    vars => {
+      service_type => 'production',
+      nrpe_command => 'check_zombie_procs',
+    },
+    assign_where => '"linux_servers" in host.groups',
+    ignore_where => 'host.name == "localhost"',
+    target_dir => '/etc/icinga2/objects/applys'
+  }
+
+  #Create an apply that checks MySQL:
+  icinga2::object::apply_service_to_host { 'check_mysql_service':
+    display_name => 'MySQL',
+    check_command => 'nrpe',
+    vars => {
+      service_type => 'production',
+      nrpe_command => 'check_mysql_service',
+    },
+    assign_where => '"mysql_servers" in host.groups',
+    ignore_where => 'host.name == "localhost"',
+    target_dir => '/etc/icinga2/objects/applys'
+  }
+
+  class { 'icinga2::nrpe':
+    nrpe_allowed_hosts => ['10.0.1.81', '10.0.1.82', '10.0.1.83', '10.0.1.84', '10.0.1.92', '127.0.0.1'],
+  }
+
+  #Some basic box health stuff
+  icinga2::nrpe::command { 'check_users':
+    nrpe_plugin_name => 'check_users',
+    nrpe_plugin_args => '-w 5 -c 10',
+  }
+  
+  #check_load
+  icinga2::nrpe::command { 'check_load':
+    nrpe_plugin_name => 'check_load',
+    nrpe_plugin_args => '-w 50,40,30 -c 60,50,40',
+  }
+  
+  #check_disk
+  icinga2::nrpe::command { 'check_disk':
+    nrpe_plugin_name => 'check_disk',
+    nrpe_plugin_args => '-w 20% -c 10% -p /',
+  }
+
+  #check_total_procs  
+  icinga2::nrpe::command { 'check_total_procs':
+    nrpe_plugin_name => 'check_procs',
+    nrpe_plugin_args => '-w 1000 -c 1500',
+  }
+  
+  #check_zombie_procs
+  icinga2::nrpe::command { 'check_zombie_procs':
+    nrpe_plugin_name => 'check_procs',
+    nrpe_plugin_args => '-w 5 -c 10 -s Z',
+  }
+  
+  icinga2::nrpe::command { 'check_ntp_time':
+    nrpe_plugin_name => 'check_ntp_time',
+    nrpe_plugin_args => '-H 127.0.0.1',
+  }
+  
+  #Create an NRPE command to monitor MySQL:
+  icinga2::nrpe::command { 'check_mysql_service':
+    nrpe_plugin_name => 'check_mysql',
+    nrpe_plugin_args => '-H 127.0.0.1 -u root -p horsebatterystaple',
+  }
+
+  ###############################
+  # collectd installation/setup
+  ###############################
+
+  #Install Collectd so we can get metrics from this machine into Riemann/InfluxDB:
+  class { '::collectd':
+    purge        => true,
+    recurse      => true,
+    purge_config => true,
+  }
+  
+  collectd::plugin { 'df': }
+  collectd::plugin { 'disk': }
+  collectd::plugin { 'entropy': }
+  collectd::plugin { 'memory': }
+  collectd::plugin { 'swap': }
+  collectd::plugin { 'cpu': }
+  collectd::plugin { 'cpufreq': }
+  collectd::plugin { 'contextswitch': }
+  collectd::plugin { 'processes': }
+  collectd::plugin { 'vmem': }
+  class { 'collectd::plugin::load':}
+  
+  #Write the collectd status to the Riemann VM in the Graphite format:
+  class { 'collectd::plugin::write_graphite':
+    graphitehost => 'dnsmetrics.local',
+    protocol => 'tcp',
+    graphiteport => 2003,
+  }
+
+  #Get network interface data:
+  class { 'collectd::plugin::interface':
+    interfaces     => ['lo', 'eth0', 'eth1'],
+  }
+
 }
