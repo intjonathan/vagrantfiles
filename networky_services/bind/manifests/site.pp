417befbae2d75bdd6ca3abd8fdd45236e2551bab
Nick Chappell <nick@intronic.org>
2014-10-26 00:21:38 -0700

    Added classes to the node definition for the DNS metrics server VM.

--- networky_services/bind/manifests/site.pp
+++ networky_services/bind/manifests/site.pp
@@ -587,6 +587,146 @@ node 'dnsmail.local' {
 
 node 'dnsmetrics.local' {
 
+  ###############################
+  # SSH installation/setup
+  ###############################
+
+ #This module is from: https://github.com/saz/puppet-ssh
+  class { 'ssh':
+    #Export host keys to PuppetDB:
+    storeconfigs_enabled => true,
+    server_options => {
+      #Whether to allow password auth; if set to 'no', only SSH keys can be used:
+      #'PasswordAuthentication' => 'no',
+      #How many authentication attempts to allow before disconnecting:
+      'MaxAuthTries'         => '10',
+      'PermitEmptyPasswords' => 'no', 
+      'PermitRootLogin'      => 'no',
+      'Port'                 => [22],
+      'PubkeyAuthentication' => 'yes',
+      #Whether to be strict about the permissions on a user's .ssh/ folder and public keys:
+      'StrictModes'          => 'yes',
+      'TCPKeepAlive'         => 'yes',
+      #Whether to do reverse DNS lookups of client IP addresses when they connect:
+      'UseDNS'               => 'no',
+    },
+  }
+
+  ###############################
+  # rsyslog installation/setup
+  ###############################
+
+  #This module is from: https://github.com/saz/puppet-rsyslog
+  class { 'rsyslog::client':
+    log_remote     => true,
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+    server         => 'riemannmaster.local',
+    port           => '514',
+  }
+
+  ###############################
+  # NTP installation/setup
+  ###############################
+
+  #This module is: https://github.com/puppetlabs/puppetlabs-ntp
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', '2.ubuntu.pool.ntp.org', '3.ubuntu.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
+  ###############################
+  # Apache installation/setup
+  ###############################
+
+  #Install Apache so we can run Kibana:
+  class{ '::apache':}
+  ::apache::mod { 'ssl': } #Install/enable the SSL module
+  ::apache::mod { 'proxy': } #Install/enable the proxy module
+  ::apache::mod { 'proxy_http': } #Install/enable the HTTP proxy module
+  ::apache::mod { 'rewrite': } #Install/enable the rewrite module
+
+  ###############################
+  # Postfix installation/setup
+  ###############################
+
+  #Install Postfix so we can monitor SMTP services and send out email alerts:
+  class { '::postfix::server':
+    inet_interfaces => 'all', #Listen on all interfaces
+    inet_protocols => 'all', #Use both IPv4 and IPv6
+    mydomain       => 'local',
+    mynetworks => '127.0.0.0/8 [::ffff:127.0.0.0]/104 [::1]/128 10.0.1.0/24',
+    extra_main_parameters => {
+      'home_mailbox' => 'Maildir/',
+      'mailbox_command' => '',
+      'disable_dns_lookups' => 'yes' #Don't do DNS lookups for MX records since we're just using /etc/hosts for all host lookups
+    }  
+  }
+
+  ###############################
+  # Riemann installation/setup
+  ###############################
+
+  #Install Java so we can run Riemann; use the -> arrow so that it gets instaleld:
+  package {'openjdk-7-jdk':
+    ensure => installed,
+  } ->
+
+  class { 'riemann': 
+    version => '0.2.6',
+    riemann_config_source => 'puppet:///riemann/configs/riemann.config',
+  }
+
+  ###############################
+  # InfluxDB installation/setup
+  ###############################
+
+
+
+  ###############################
+  # Grafana installation/setup
+  ###############################
+
+
+
+  ###############################
+  # collectd installation/setup
+  ###############################
+
+  #Install Collectd so we can get metrics from this machine into Riemann/InfluxDB:
+  class { '::collectd':
+    purge        => true,
+    recurse      => true,
+    purge_config => true,
+  }
+  
+  collectd::plugin { 'df': }
+  collectd::plugin { 'disk': }
+  collectd::plugin { 'entropy': }
+  collectd::plugin { 'memory': }
+  collectd::plugin { 'swap': }
+  collectd::plugin { 'cpu': }
+  collectd::plugin { 'cpufreq': }
+  collectd::plugin { 'contextswitch': }
+  collectd::plugin { 'processes': }
+  collectd::plugin { 'vmem': }
+  class { 'collectd::plugin::load':}
+  
+  #Write the collectd status to the Riemann VM in the Graphite format:
+  class { 'collectd::plugin::write_graphite':
+    graphitehost => 'dnsmetrics.local',
+    protocol => 'tcp',
+    graphiteport => 2003,
+  }
+
+  #Get network interface data:
+  class { 'collectd::plugin::interface':
+    interfaces     => ['lo', 'eth0', 'eth1'],
+  }
+
 }
 
 node 'dnslogging.local' {
