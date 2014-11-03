52c443b6a668153db4a8bbbcbd4001bca3e8ee77
Nick Chappell <nick@intronic.org>
2014-11-02 19:44:34 -0800

    Install Logstash on the BIND DNS monitoring VM.

--- networky_services/bind/manifests/site.pp
+++ networky_services/bind/manifests/site.pp
@@ -1409,6 +1409,24 @@ node 'dnsmonitoring.local' {
   }
 
   ###############################
+  # Logstash installation/setup
+  ###############################
+
+  #Install Logstash:  
+  class { 'logstash':
+    java_install => true,
+    java_package => 'openjdk-7-jre-headless',
+    package_url => 'https://download.elasticsearch.org/logstash/logstash/packages/debian/logstash_1.4.2-1-2c0f5a1_all.deb',
+    install_contrib => true,
+    contrib_package_url => 'https://download.elasticsearch.org/logstash/logstash/packages/debian/logstash-contrib_1.4.2-1-efd53ef_all.deb',
+  }
+
+  logstash::configfile { 'logstash_monolithic':
+    source => 'puppet:///logstash/configs/logstash.conf',
+    order   => 10
+  }
+
+  ###############################
   # rsyslog installation/setup
   ###############################
 
