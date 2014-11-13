5919552508715b5b10e15d7fa3ebc4330f6951bb
Nick Chappell <nick@intronic.org>
2014-11-12 21:19:27 -0800

    Purge unmanaged NRPE config files on trustyicinga2client.

--- monitoring/icinga2/manifests/site.pp
+++ monitoring/icinga2/manifests/site.pp
@@ -2232,6 +2232,7 @@ node 'trustyicinga2client.local' {
 
   class { 'icinga2::nrpe':
     nrpe_allowed_hosts => ['10.0.1.81', '10.0.1.82', '10.0.1.83', '10.0.1.84', '10.0.1.92', '127.0.0.1'],
+    nrpe_purge_unmanaged => true,
   }
 
   #Some basic box health stuff
