7a6aeb7238e036167c5d4370c039839b28cad603
Nick Chappell <nick@intronic.org>
2014-11-30 21:28:25 -0800

    Added a node definition in the site.pp manifest for heka3.

--- metrics/heka/production/manifests/site.pp
+++ metrics/heka/production/manifests/site.pp
@@ -114,6 +114,36 @@ node 'heka2.local' {
 
 }
 
+node 'heka3.local' {
+
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #This module is from: https://github.com/saz/puppet-rsyslog
+  class { 'rsyslog::client':
+    #Write out logs in RFC3146 format so that they're more consistent when we send them to
+    #Logstash. Logstash is set up to understand this format of logs in its config:
+    log_templates => [
+      { name => 'RFC3164fmt', template => '<%PRI%>%TIMESTAMP% %HOSTNAME% %syslogtag%%msg%',},
+    ],
+    log_remote     => true,
+    server         => 'hekalogging.local',
+    port           => '5514',
+    remote_type    => 'tcp',
+    log_local      => true,
+    log_auth_local => true,
+    custom_config  => undef,
+  }
+
+  #This module is: https://github.com/puppetlabs/puppetlabs-ntp
+  class { '::ntp':
+    servers  => [ '0.ubuntu.pool.ntp.org', '1.ubuntu.pool.ntp.org', '2.ubuntu.pool.ntp.org', '3.ubuntu.pool.ntp.org' ],
+    restrict => ['127.0.0.1', '10.0.1.0 mask 255.255.255.0 kod notrap nomodify nopeer noquery'],
+    disable_monitor => true,
+  }
+
+}
+
 node 'collectd1.local' {
 
   #Install Apache so we test collectd's Apache metrics gathering.
