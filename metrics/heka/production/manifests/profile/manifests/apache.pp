b3a5d3af30bf6f504504b7fada0d30d44d7e3492
Nick Chappell <nick@intronic.org>
2014-11-30 21:44:55 -0800

    Add more Apache modules in the Apache profile.

--- metrics/heka/production/manifests/profile/manifests/apache.pp
+++ metrics/heka/production/manifests/profile/manifests/apache.pp
@@ -2,12 +2,30 @@
 
 class profile::apache {
 
+  #This is the Puppet Labs Apache module: https://github.com/puppetlabs/puppetlabs-apache
+  class { '::apache': 
+    purge_configs => 'true'
+  }  
+
+  ::apache::mod { 'ssl': } #Install/enable the SSL module
+  ::apache::mod { 'proxy': } #Install/enable the proxy module
+  ::apache::mod { 'proxy_http': } #Install/enable the HTTP proxy module
+  ::apache::mod { 'headers': }
+
 }
 
 class profile::apache::wsgi {
 
-  #
-  class { '::apache': }
+  #This is the Puppet Labs Apache module: https://github.com/puppetlabs/puppetlabs-apache
+  class { '::apache': 
+    purge_configs => 'true'
+  }  
+
+  ::apache::mod { 'ssl': } #Install/enable the SSL module
+  ::apache::mod { 'proxy': } #Install/enable the proxy module
+  ::apache::mod { 'proxy_http': } #Install/enable the HTTP proxy module
+  ::apache::mod { 'headers': }
+
   class { '::apache::mod::wsgi': }
 
 }
