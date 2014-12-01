78cbeec97d4947b347a2cf67c6321797fce4868c
Nick Chappell <nick@intronic.org>
2014-11-30 19:57:37 -0800

    Moved the rsyslog class application to the rsyslog::client profile.

--- metrics/heka/production/manifests/site.pp
+++ metrics/heka/production/manifests/site.pp
@@ -23,21 +23,9 @@ node 'hekamaster.local' {
     port => 80,
   }
  
-  #This module is from: https://github.com/saz/puppet-rsyslog
-  class { 'rsyslog::client':
-    #Write out logs in RFC3146 format so that they're more consistent when we send them to
-    #Logstash. Logstash is set up to understand this format of logs in its config:
-    log_templates => [
-      { name => 'RFC3164fmt', template => '<%PRI%>%TIMESTAMP% %HOSTNAME% %syslogtag%%msg%',},
-    ],
-    log_remote     => true,
-    server         => 'hekalogging.local',
-    port           => '5514',
-    remote_type    => 'tcp',
-    log_local      => true,
-    log_auth_local => true,
-    custom_config  => undef,
-  }
+  #Include the rsyslog::client profile to set up logging
+
+  include profile::rsyslog::client
 
   #Include a profile that sets up our usual SSH settings:
   include profile::ssh
