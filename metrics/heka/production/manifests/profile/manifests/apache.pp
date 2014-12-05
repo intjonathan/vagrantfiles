f7bc5acc5397753ad76455bf1f69d009b4f0bc60
Nick Chappell <nick@intronic.org>
2014-12-04 22:09:08 -0800

    Add the Apache base class stuff back into the WSGI profile since the WSGI Puppet code needs the base Apache class applied too.

--- metrics/heka/production/manifests/profile/manifests/apache.pp
+++ metrics/heka/production/manifests/profile/manifests/apache.pp
@@ -39,6 +39,16 @@ class profile::apache {
 
 class profile::apache::wsgi {
 
+  #This is the Puppet Labs Apache module: https://github.com/puppetlabs/puppetlabs-apache
+  class { '::apache': 
+    purge_configs => 'true'
+  }  
+
+  ::apache::mod { 'ssl': } #Install/enable the SSL module
+  ::apache::mod { 'proxy': } #Install/enable the proxy module
+  ::apache::mod { 'proxy_http': } #Install/enable the HTTP proxy module
+  ::apache::mod { 'headers': }
+
   class { '::apache::mod::wsgi': }
 
 }
