2194245627fd35c00d1235f10347a4ec0791b118
Nick Chappell <nick@intronic.org>
2014-12-04 21:46:17 -0800

    Move the Logstash configuration to a profile and apply the profile to hekalogging

--- metrics/heka/production/manifests/site.pp
+++ metrics/heka/production/manifests/site.pp
@@ -211,19 +211,9 @@ node 'hekalogging.local' {
   #Install Java...
   include profile::java
 
-  #Install Logstash:  
-  class { 'logstash':
-    java_install => true,
-    java_package => 'openjdk-7-jre-headless',
-    package_url => 'https://download.elasticsearch.org/logstash/logstash/packages/debian/logstash_1.4.2-1-2c0f5a1_all.deb',
-    install_contrib => true,
-    contrib_package_url => 'https://download.elasticsearch.org/logstash/logstash/packages/debian/logstash-contrib_1.4.2-1-efd53ef_all.deb',
-  }
-
-  logstash::configfile { 'logstash_monolithic':
-    source => 'puppet:///logstash/configs/logstash.conf',
-    order   => 10
-  }
+  #Include Logstash
+  include profile::logstash
+  include profile::logstash::config
 
   #Include Elasticsearch
   include profile::elasticsearch
