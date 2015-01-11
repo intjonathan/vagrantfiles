343828f0e9a5f42cdfb612cd62e1b33408441bc4
Nick Chappell <nick@intronic.org>
2015-01-10 19:02:13 -0800

    Add MySQL installation to the MySQL profile.

---
+++ monitoring/icinga2/production/manifests/profile/manifests/mysql.pp
@@ -0,0 +1,8 @@
+class profile::mysql {
+
+  class { '::mysql::server':
+    root_password    => 'horsebatterystaple',
+    override_options => { 'mysqld' => { 'max_connections' => '1024' } }
+  }
+
+}
