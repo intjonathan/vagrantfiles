4a3ecd689b61ef74f9dd5c2394005146acb08850
Nick Chappell <nick@intronic.org>
2015-01-10 18:12:58 -0800

    Add in NTP, rsyslog and SSH profiles to the VM node definitions.

--- monitoring/icinga2/production/manifests/site.pp
+++ monitoring/icinga2/production/manifests/site.pp
@@ -24,71 +24,206 @@ node 'icinga2master.local' {
 #An Ubuntu 14.04 Icinga2 server node
 node 'trustyicinga2server.local' { 
 
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include the rsyslog::client profile to set up logging
+  include profile::rsyslog::client
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
+
 }
 
 #An Ubuntu 14.10 Icinga2 server node
 node 'utopicicinga2server.local' {
+
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include the rsyslog::client profile to set up logging
+  include profile::rsyslog::client
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
 
 }
 
 #An Ubuntu 12.04 Icinga2 server node
 node 'preciseicinga2server.local' {
 
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include the rsyslog::client profile to set up logging
+  include profile::rsyslog::client
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
+
 }
 
 #CentOS 6 Icinga 2 server node
 node 'centos6icinga2server.local' {
+
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include the rsyslog::client profile to set up logging
+  include profile::rsyslog::client
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
 
 }
 
 #A CentOS 7 Icinga 2 server node
 node 'centos7icinga2server.local' {
 
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include the rsyslog::client profile to set up logging
+  include profile::rsyslog::client
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
+
 }
 
 #A Debian 7 Icinga2 server node
 node 'debian7icinga2server.local' {
+
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include the rsyslog::client profile to set up logging
+  include profile::rsyslog::client
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
 
 }
 
 #An Ubuntu 14.04 Icinga 2 client node
 node 'trustyicinga2client.local' {
 
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include the rsyslog::client profile to set up logging
+  include profile::rsyslog::client
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
+
 }
 
 #An Ubuntu 14.10 Icinga 2 client node
 node 'utopicicinga2client.local' {
+
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include the rsyslog::client profile to set up logging
+  include profile::rsyslog::client
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
 
 }
 
 #An Ubuntu 12.04 Icinga 2 client node
 node 'preciseicinga2client.local' {
 
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include the rsyslog::client profile to set up logging
+  include profile::rsyslog::client
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
+
 }
 
 #A CentOS 6 Icinga 2 client node
 node 'centos6icinga2client.local' {
+
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include the rsyslog::client profile to set up logging
+  include profile::rsyslog::client
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
 
 }
 
 #A CentOS 7 Icinga 2 client node
 node 'centos7icinga2client.local' {
 
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include the rsyslog::client profile to set up logging
+  include profile::rsyslog::client
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
+
 }
 
 #A Debian 7 Icinga 2 client node
 node 'debian7icinga2client.local' {
 
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include the rsyslog::client profile to set up logging
+  include profile::rsyslog::client
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
+
 }
 
 node 'icinga2mail.local' {
+
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include the rsyslog::client profile to set up logging
+  include profile::rsyslog::client
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
 
 }
 
 node 'usermail.local' {
 
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include the rsyslog::client profile to set up logging
+  include profile::rsyslog::client
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
+
 }
 
 node 'icinga2logging.local' {
+
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include the rsyslog::client profile to set up logging
+  include profile::rsyslog::client
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
 
 }
