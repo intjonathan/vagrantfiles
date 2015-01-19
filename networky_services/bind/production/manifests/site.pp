f2879d0758a2d3c0233b47c0fcfc1130362fb521
Nick Chappell <nick@intronic.org>
2015-01-18 16:52:15 -0800

    Corrected file names and paths that were Debian/Ubuntu BIND packages.

--- networky_services/bind/production/manifests/site.pp
+++ networky_services/bind/production/manifests/site.pp
@@ -93,20 +93,24 @@ node 'dnsmaster1.local' {
       'local'   => [ '127.0.0.1' ],
       '10net'   => [ '10.0.0.0/24', '10.0.1.0/24', '10.1.1.0/24', '10.1.0.0/24'],
     },
-    directory => '/etc/bind/',
+    directory => '/var/named/',
     listen_on_addr    => [ '127.0.0.1' ],
     listen_on_v6_addr => [ '::1' ],
     forwarders        => [ '8.8.8.8', '8.8.4.4' ],
     allow_query       => [ 'localhost', 'local' ],
     recursion         => 'no',
     allow_recursion   => [ 'localhost', 'local', '10net'],
+    #Specify a managed keys directory; BIND needs this specified in /etc/named.conf or it 
+    #won't be able to write to it; unfortunately, the module has the value default to 'undef'
+    #and won't print it in named.conf if nothing is specified
+    managed_keys_directory => '/var/named/dynamic',
     #Include some other zone files for localhost and loopback zones:
-    includes => ['/etc/bind/named.conf.local', '/etc/bind/named.conf.default-zones'],
+    includes => ['/etc/named.rfc1912.zones', '/etc/named.root.key'],
     zones => {
       #root hints zone
       '.' => [
         'type hint',
-        'file "/etc/bind/db.root"',
+        'file "named.ca"',
       ], 
       'zone1.local' => [
       'type master',
@@ -209,13 +213,13 @@ node 'dnsmaster2.local' {
 
   #BIND module is from: https://github.com/thias/puppet-bind
   include bind
-  bind::server::conf { '/etc/bind/named.conf':
+  bind::server::conf { '/etc/named.conf':
     acls => {
       'rfc1918' => [ '10/8', '172.16/12', '192.168/16' ],
       'local'   => [ '127.0.0.1' ],
       '10net'   => [ '10.0.0.0/24', '10.0.1.0/24', '10.1.1.0/24', '10.1.0.0/24'],
     },
-    directory => '/etc/bind/',
+    directory => '/var/named/',
     listen_on_addr    => [ '127.0.0.1' ],
     listen_on_v6_addr => [ '::1' ],
     forwarders        => [ '8.8.8.8', '8.8.4.4' ],
