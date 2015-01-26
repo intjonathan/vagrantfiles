b7641ea67168dfc1d159dfa21aba350a3cb186cd
Nick Chappell <nick@intronic.org>
2015-01-25 17:10:08 -0800

    Create an ssl/ folder for Dovecot to store its cert and key in.

--- networky_services/mail/production/manifests/profile/manifests/dovecot.pp
+++ networky_services/mail/production/manifests/profile/manifests/dovecot.pp
@@ -1,5 +1,13 @@
 class profile::dovecot {
 
+  #Create a folder for the SSL cert and private key:
+  file { '/etc/dovecot/ssl':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0400',
+  }
+
   class { '::dovecot': 
     mail_location => 'maildir:~/Maildir',
   }
