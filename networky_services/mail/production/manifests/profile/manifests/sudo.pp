80b57450958306dd4661508447f1b8c0e1f5d43d
Nick Chappell <nick@intronic.org>
2015-01-25 17:23:43 -0800

    Added a sudoers rule for the vagrant user.

--- networky_services/mail/production/manifests/profile/manifests/sudo.pp
+++ networky_services/mail/production/manifests/profile/manifests/sudo.pp
@@ -6,10 +6,16 @@ class profile::sudo {
     content  => "%admin ALL=(ALL) NOPASSWD: ALL",
   }
 
-
-  ::sudo::conf { 'sduo':
+  ::sudo::conf { 'sudo':
     priority => 20,
     content  => "%sudo ALL=(ALL) NOPASSWD: ALL",
+  }
+
+  #Create a sudoers rule for the vagrant user so it can run sudo commands without needing
+  #to give a password:
+  ::sudo::conf { 'vagrant':
+    priority => 20,
+    content  => "vagrant ALL=NOPASSWD:ALL",
   }
 
 }
