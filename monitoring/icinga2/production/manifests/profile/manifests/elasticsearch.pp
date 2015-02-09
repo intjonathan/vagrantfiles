97f713f71ecca354a82b0777d260fb58cb54386d
Nick Chappell <nick@intronic.org>
2015-02-08 21:35:38 -0800

    Corrected the Elasticsearch cluster name and  Logstash ES bind IP address.

--- monitoring/icinga2/production/manifests/profile/manifests/elasticsearch.pp
+++ monitoring/icinga2/production/manifests/profile/manifests/elasticsearch.pp
@@ -3,7 +3,7 @@ class profile::elasticsearch {
     class { '::elasticsearch':
     java_install => false,
     package_url => 'https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.1.deb',
-    config => { 'cluster.name'             => 'heka',
+    config => { 'cluster.name'             => 'logstash',
                 'network.host'             => $ipaddress_eth1,
                 'index.number_of_replicas' => '1',
                 'index.number_of_shards'   => '4',
