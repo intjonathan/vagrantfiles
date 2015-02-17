5757c938aedff9c10189e8d530bb1d15c4398b37
Nick Chappell <nick@intronic.org>
2015-02-16 19:09:59 -0800

    Include some of the base profiles on each VM.

--- logging/logstash/production/manifests/site.pp
+++ logging/logstash/production/manifests/site.pp
@@ -30,6 +30,12 @@ node 'logstashmaster.local' {
 
 node 'logstash.local' {
  
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
+ 
   #Install Apache so we use Kibana 3:
   include profile::apache
 
@@ -43,14 +49,23 @@ node 'logstash.local' {
   #Make this machine a Consul server:
   include profile::consul::server
 
+  #Include a profile that installs and configures Postfix:
+  include profile::postfix::server
+
 }
 
 node 'kibana.local' {
+
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
   
   #Include the rsyslog::client profile to set up logging
   include profile::rsyslog::client
 
-  #Install Apache so we use Kibana 3:
+  #Install Apache so we use Kibana 3 and 4:
   include profile::apache
 
   #Include the profile that sets up a virtual host for Kibana 3:
@@ -59,9 +74,6 @@ node 'kibana.local' {
   #Include the profile that sets up a virtual host for Kibana 4:
   include profile::kibana4::apache_virtualhost
 
-  #Make this machine a Consul client:
-  include profile::consul::client
-
   #Make this machine a Consul server:
   include profile::consul::server
 
@@ -69,58 +81,130 @@ node 'kibana.local' {
 
 node 'elasticsearch1.local' {
 
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
+
   #Include Elasticsearch
   include profile::elasticsearch
 
   #Install Java...
   include profile::java
+
+  #Make this machine a Consul client:
+  include profile::consul::client
 
 }
 
 node 'elasticsearch2.local' {
 
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
+
   #Include Elasticsearch
   include profile::elasticsearch
 
   #Install Java...
   include profile::java
+
+  #Make this machine a Consul client:
+  include profile::consul::client
 
 }
 
 node 'elasticsearch3.local' {
 
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
+
   #Include Elasticsearch
   include profile::elasticsearch
 
   #Install Java...
   include profile::java
 
+  #Make this machine a Consul client:
+  include profile::consul::client
+
 }
 
 node 'rsyslog1.local' {
 
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
+
   #Include the rsyslog::client profile to set up logging
   include profile::rsyslog::client
+
+  #Make this machine a Consul client:
+  include profile::consul::client
 
 }
 
 node 'rsyslog2.local' {
 
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
+
   #Include the rsyslog::client profile to set up logging
   include profile::rsyslog::client
+
+  #Make this machine a Consul client:
+  include profile::consul::client
 
 }
 
 node 'rsyslog3.local' {
 
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
+
   #Include the rsyslog::client profile to set up logging
   include profile::rsyslog::client
+
+  #Make this machine a Consul client:
+  include profile::consul::client
 
 }
 
 node 'logstashmetrics.local' {
 
+  #Include a profile that sets up our usual SSH settings:
+  include profile::ssh
+
+  #Include a profile that sets up NTP
+  include profile::ntp::client
+
   #Include the rsyslog::client profile to set up logging
   include profile::rsyslog::client
+
+  #Make this machine a Consul client:
+  include profile::consul::client
+
+  #Include a profile that installs and configures Postfix:
+  include profile::postfix::server
+
+  #Install Apache so we use Grafana:
+  include profile::apache
+
+  #Include the Grafana profile
+  include profile::grafana
 
 }
