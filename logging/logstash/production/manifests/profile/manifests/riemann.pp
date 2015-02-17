0206eafd1dd29df2a2e5c5f90aa450229ffc40bc
Nick Chappell <nick@intronic.org>
2015-02-16 19:11:59 -0800

    Update the version of Riemann.

--- logging/logstash/production/manifests/profile/manifests/riemann.pp
+++ logging/logstash/production/manifests/profile/manifests/riemann.pp
@@ -1,7 +1,7 @@
 class profile::riemann {
 
   class { '::riemann': 
-    version => '0.2.6',
+    version => '0.2.8',
     riemann_config_source => 'puppet:///riemann/configs/riemann.config',
   }
 
