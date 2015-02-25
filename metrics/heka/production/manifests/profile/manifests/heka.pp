9b44276baad753fb616a77ec149e2db5a5c23bb5
Nick Chappell <nick@intronic.org>
2015-02-25 15:09:56 -0800

    Add some TCP and UDP Heka inputs.

--- metrics/heka/production/manifests/profile/manifests/heka.pp
+++ metrics/heka/production/manifests/profile/manifests/heka.pp
@@ -2,5 +2,26 @@ class profile::heka {
 
   class { '::heka':
   }
-  
+
+
+  ::heka::plugin::input::tcpinput { 'tcpinput1':
+    refresh_heka_service => true,
+    address => "${::ipaddress_lo}:5565"
+  }
+
+  ::heka::plugin::input::tcpinput { 'tcpinput2':
+    refresh_heka_service => true,
+    address => "${::ipaddress_lo}:5566"
+  }
+
+  ::heka::plugin::input::tcpinput { 'tcpinput3':
+    refresh_heka_service => true,
+    address => "${::ipaddress_lo}:5567"
+  }
+
+  ::heka::plugin::input::udpinput { 'udpinput1':
+    refresh_heka_service => true,
+    address => "${::ipaddress_lo}:4880"
+  }
+
 }
