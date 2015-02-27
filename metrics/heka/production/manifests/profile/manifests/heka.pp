fec3c003fc40e5ef452c406d301c8504bc848a4f
Nick Chappell <nick@intronic.org>
2015-02-26 16:29:45 -0800

    Added more Heka plugin definitions.

--- metrics/heka/production/manifests/profile/manifests/heka.pp
+++ metrics/heka/production/manifests/profile/manifests/heka.pp
@@ -24,4 +24,39 @@ class profile::heka {
     address => "${::ipaddress_lo}:4880"
   }
 
+  ::heka::plugin { 'tcpinput4':
+    refresh_heka_service => true,
+    type => 'TcpInput',
+    settings => {
+      'address' => '"127.0.0.1:5568"',
+      'parser_type' => '"token"',
+    },
+  }
+
+  ::heka::plugin { 'dashboard1':
+    type => 'DashboardOutput',
+    settings => {
+      'address' => '"10.0.1.120:4352"',
+      'ticker_interval' => 6,
+    },
+  }
+
+  ::heka::plugin { 'dashboard2':
+    type => 'DashboardOutput',
+    settings => {
+      'address' => '"10.0.1.120:4353"',
+      'ticker_interval' => 6,
+    },
+  }
+
+  ::heka::plugin { 'dashboard3':
+    type => 'DashboardOutput',
+    settings => {
+      'address' => '"10.0.1.120:4354"',
+      'ticker_interval' => 6,
+    },
+  }
+
+
+
 }
