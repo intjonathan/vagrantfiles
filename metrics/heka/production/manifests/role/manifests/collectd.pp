5e65e17beb76cbb00242287a0ac33411412bed6a
Nick Chappell <nick@intronic.org>
2015-03-29 14:52:41 -0700

    Don't apply the CollectD NTP profile to the CollectD role; we don't want to gather NTP metrics since most of them are empty (NaN in Graphite's terminology).

--- metrics/heka/production/manifests/role/manifests/collectd.pp
+++ metrics/heka/production/manifests/role/manifests/collectd.pp
@@ -8,7 +8,9 @@ class role::collectd::collectd_system_and_ntp_metrics_and_write_graphite {
 
   include profile::collectd
   include profile::collectd::system_metrics
-  include profile::collectd::ntp_metrics
+  #Don't include the NTP CollectD profile, as most of the carbon metrics it generates are 
+  #NaN, meaning they don't have any value. 
+  #include profile::collectd::ntp_metrics
   include profile::collectd::write_graphite
 
 }
