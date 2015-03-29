15e3baf6564d9b1249cafd8cdc511ee4947e7dc1
Nick Chappell <nick@intronic.org>
2015-03-28 18:29:52 -0700

    Use the synchronous InfluxDB writer since it's been improved in Riemann 0.2.9.

--- metrics/heka/production/manifests/profile/manifests/consul.pp
+++ metrics/heka/production/manifests/profile/manifests/consul.pp
@@ -9,45 +9,103 @@ class profile::consul::agent {
 
 class profile::consul::server {
 
-  class { '::consul':
-    version => '0.5.0',
-    config_hash => {
-      'bind_addr'             => $ipaddress_eth1,
-      'bootstrap_expect'      => 3,
-      'check_update_interval' => '2m',
-      'retry_join'            => ['10.0.1.110'],
-      'retry_interval'        => '10s',
-      'client_addr'           => '0.0.0.0',
-      'data_dir'              => '/opt/consul',
-      'ui_dir'                => '/opt/consul/ui',
-      'datacenter'            => 'heka',
-      'enable_syslog'         => true,
-      'log_level'             => 'INFO',
-      'node_name'             => $fqdn,
-      'server'                => true,
-      'statsd_addr'           => '127.0.0.1',
+  #Get the address of the host-only interface by looking up the right Facter fact for
+  #it to get the right address. I need to do this because CentOS 7 has device names like
+  #'enp0s8' as opposed to 'eth0' or 'eth1':
+  if $operatingsystem == 'CentOS' and $operatingsystemmajrelease == '7' {
+    class { '::consul':
+      version => '0.5.0',
+      config_hash => {
+        'bind_addr'             => $::ipaddress_enp0s8,
+        'bootstrap_expect'      => 3,
+        'check_update_interval' => '2m',
+        'retry_join'            => ['10.0.1.110'],
+        'retry_interval'        => '10s',
+        'client_addr'           => '0.0.0.0',
+        'data_dir'              => '/opt/consul',
+        'ui_dir'                => '/opt/consul/ui',
+        'datacenter'            => 'heka',
+        'enable_syslog'         => true,
+        'log_level'             => 'INFO',
+        'node_name'             => $fqdn,
+        'server'                => true,
+        'statsd_addr'           => '127.0.0.1',
+      }
     }
+  } 
+  #If it's not CentOS 7, just use ipaddress_eth1:
+  else {
+    class { '::consul':
+      version => '0.5.0',
+      config_hash => {
+        'bind_addr'             => $::ipaddress_eth1,
+        'bootstrap_expect'      => 3,
+        'check_update_interval' => '2m',
+        'retry_join'            => ['10.0.1.110'],
+        'retry_interval'        => '10s',
+        'client_addr'           => '0.0.0.0',
+        'data_dir'              => '/opt/consul',
+        'ui_dir'                => '/opt/consul/ui',
+        'datacenter'            => 'heka',
+        'enable_syslog'         => true,
+        'log_level'             => 'INFO',
+        'node_name'             => $fqdn,
+        'server'                => true,
+        'statsd_addr'           => '127.0.0.1',
+      }
+    } 
   }
 
 }
 
 class profile::consul::client {
 
-  class { '::consul':
-    version => '0.5.0',
-    config_hash => {
-      'bind_addr'             => $ipaddress_eth1,
-      'start_join'            => ['10.0.1.110'],
-      'check_update_interval' => '2m',
-      'retry_join'            => ['10.0.1.110'],
-      'retry_interval'        => '10s',
-      'client_addr'           => '0.0.0.0',
-      'data_dir'              => '/opt/consul',
-      'datacenter'            => 'heka',
-      'enable_syslog'         => true,
-      'log_level'             => 'INFO',
-      'node_name'             => $fqdn,
-      'statsd_addr'           => '127.0.0.1',
+  #Get the address of the host-only interface by looking up the right Facter fact for
+  #it to get the right address. I need to do this because CentOS 7 has device names like
+  #'enp0s8' as opposed to 'eth0' or 'eth1':
+  if $operatingsystem == 'CentOS' and $operatingsystemmajrelease == '7' {
+    class { '::consul':
+      version => '0.5.0',
+      config_hash => {
+        #Get the address of the host-only interface by looking up the right Facter fact for
+        #it to get the right address. I need to do this because CentOS 7 has device names like
+        #'enp0s8' as opposed to 'eth0' or 'eth1'
+        'bind_addr'             => $::ipaddress_enp0s8,
+        'start_join'            => ['10.0.1.110'],
+        'check_update_interval' => '2m',
+        'retry_join'            => ['10.0.1.110'],
+        'retry_interval'        => '10s',
+        'client_addr'           => '0.0.0.0',
+        'data_dir'              => '/opt/consul',
+        'datacenter'            => 'heka',
+        'enable_syslog'         => true,
+        'log_level'             => 'INFO',
+        'node_name'             => $fqdn,
+        'statsd_addr'           => '127.0.0.1',
+      }
+    }
+  } 
+  #If it's not CentOS 7, just use ipaddress_eth1:
+  else {
+    class { '::consul':
+      version => '0.5.0',
+      config_hash => {
+        #Get the address of the host-only interface by looking up the right Facter fact for
+        #it to get the right address. I need to do this because CentOS 7 has device names like
+        #'enp0s8' as opposed to 'eth0' or 'eth1'
+        'bind_addr'             => $::ipaddress_eth1,
+        'start_join'            => ['10.0.1.110'],
+        'check_update_interval' => '2m',
+        'retry_join'            => ['10.0.1.110'],
+        'retry_interval'        => '10s',
+        'client_addr'           => '0.0.0.0',
+        'data_dir'              => '/opt/consul',
+        'datacenter'            => 'heka',
+        'enable_syslog'         => true,
+        'log_level'             => 'INFO',
+        'node_name'             => $fqdn,
+        'statsd_addr'           => '127.0.0.1',
+      }
     }
   }
 
