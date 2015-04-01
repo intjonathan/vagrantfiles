e8c536c1ff630f28de0488f83c87bf8d70dfc7d6
Nick Chappell <nick@intronic.org>
2015-03-31 21:33:16 -0700

    Added rsyslog imifile log rules to get Cassandra logs into rsyslog.

--- databases_and_message_queues/cassandra/production/manifests/profile/manifests/cassandra.pp
+++ databases_and_message_queues/cassandra/production/manifests/profile/manifests/cassandra.pp
@@ -1,3 +1,17 @@
 class profile::cassandra {
 
+  rsyslog::imfile { 'cassandra':
+    file_name => '/var/log/cassandra/cassandra.log',
+    file_tag => 'cassandra',
+    file_facility => 'local7',
+    file_severity => 'info',
+  }
+
+  rsyslog::imfile { 'cassandra-system':
+    file_name => '/var/log/cassandra/system.log',
+    file_tag => 'cassandra-system',
+    file_facility => 'local7',
+    file_severity => 'info',
+  }
+
 }
