fe45f5b0e442cbb984d39a6395971cb61cb95aa2
Nick Chappell <nick@intronic.org>
2015-04-04 17:32:15 -0700

    Add calls to the Cassandra module to the Cassandra profile.

--- databases_and_message_queues/cassandra/production/manifests/profile/manifests/cassandra.pp
+++ databases_and_message_queues/cassandra/production/manifests/profile/manifests/cassandra.pp
@@ -1,5 +1,15 @@
 class profile::cassandra {
 
+  #JNA is Java Native Access; it prevents Linux from swapping out the JVM:
+  # http://docs.datastax.com/en/cassandra/2.0/cassandra/install/installJnaRHEL.html
+  package { 'jna':
+    ensure => installed,
+  }
+
+}
+
+class profile::cassandra::logging {
+
   rsyslog::imfile { 'cassandra':
     file_name => '/var/log/cassandra/cassandra.log',
     file_tag => 'cassandra',
@@ -12,6 +22,48 @@ class profile::cassandra {
     file_tag => 'cassandra-system',
     file_facility => 'local7',
     file_severity => 'info',
+  }
+
+}
+
+class profile::cassandra::seed {
+
+  #Include the base Cassandra profile so we can get JNA installed:
+  include profile::cassandra
+  
+  #Include the logging profile so we can get logs into rsyslog:
+  include profile::cassandra::logging
+
+  class { '::cassandra':
+    cluster_name      => 'cassandra1',
+    package_name      => 'dsc20',
+    seeds             => [hiera('cassandra_seed_node')],
+    version           => '2.0.14-1',
+    listen_address    => $::ipaddress_enp0s8,
+    rpc_address       => $::ipaddress_enp0s8,
+    rpc_min_threads   => 16,
+    rpc_max_threads   => 2048,
+  }
+
+}
+
+class profile::cassandra::node {
+
+  #Include the base Cassandra profile so we can get JNA installed:
+  include profile::cassandra
+  
+  #Include the logging profile so we can get logs into rsyslog:
+  include profile::cassandra::logging
+
+  class { '::cassandra':
+    cluster_name      => 'cassandra1',
+    package_name      => 'dsc20',
+    seeds             => [hiera('cassandra_seed_node')],
+    version           => '2.0.14-1',
+    listen_address    => $::ipaddress_enp0s8,
+    rpc_address       => $::ipaddress_enp0s8,
+    rpc_min_threads   => 16,
+    rpc_max_threads   => 2048,
   }
 
 }
