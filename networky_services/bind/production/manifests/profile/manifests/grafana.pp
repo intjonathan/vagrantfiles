ccbb053b720caa1b4446c4bfa58e65761cbd0c7f
Nick Chappell <nick@intronic.org>
2015-04-26 21:49:00 -0700

    Updated the Grafana profile.

--- networky_services/bind/production/manifests/profile/manifests/grafana.pp
+++ networky_services/bind/production/manifests/profile/manifests/grafana.pp
@@ -3,7 +3,9 @@ class profile::grafana {
   #A non-SSL virtual host for grafana:
   ::apache::vhost { "grafana.${fqdn}_non-ssl":
     port            => 80,
-    docroot         => '/sites/apps/grafana',
+    #This has to be grafana/grafana so that we don't get duplicated resource errrors from
+    #/sites/apps/grafana/ being used by this virtual host resource and by the ::grafana class below: 
+    docroot         => '/sites/apps/grafana/grafana',
     servername      => "grafana.${fqdn}",
     access_log => true,
     access_log_syslog=> 'syslog:local1',
@@ -17,6 +19,33 @@ class profile::grafana {
         Options -Multiviews
       </Directory>
     ',
+  }
+  
+  #Install Grafana with this module:
+  # https://github.com/bfraser/puppet-grafana
+  class { '::grafana':
+    version => '1.9.1',
+    #This will install Grafana to /sites/apps/grafana/grafana
+    #/sites/apps/grafana/grafana will be symlinked to something like /sites/apps/grafana/grafana-1.9.1
+    install_dir => '/sites/apps/grafana',
+    grafana_user => 'www-data',
+    grafana_group => 'www-data',
+    datasources  => {
+      'influxdbdata' => {
+        'type'    => 'influxdb',
+        'url'     => 'http://dnsmonitoring.local:8086/db/riemann-data',
+        'username' => 'nick',
+        'password' => 'password',
+        'default' => 'true',
+      },
+      'grafanadb' => {
+        'type'      => 'influxdb',
+        'url'       => 'http://dnsmonitoring.local:8086/db/grafana',
+        'username' => 'nick',
+        'password' => 'password',
+        'grafanaDB' => 'true',
+      },
+    }
   }
 
 }
