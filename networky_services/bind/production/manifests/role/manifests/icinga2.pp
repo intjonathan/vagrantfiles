b5b940556a1c1abd3ea6ed0869ce73642f6dbab4
Nick Chappell <nick@intronic.org>
2015-05-18 22:28:01 -0700

    Whitespace.

--- networky_services/bind/production/manifests/role/manifests/icinga2.pp
+++ networky_services/bind/production/manifests/role/manifests/icinga2.pp
@@ -8,12 +8,9 @@ class role::icinga2::server {
   include profile::icinga2::server
   include profile::icinga2::server::postgresql_db
   include profile::icinga2::server::objects
-  
   include profile::icingaweb2::postgresql_db
   include profile::icingaweb2::mysql_db
-  
   include profile::mysql::server
-
   include profile::postfix::server
   
 }
