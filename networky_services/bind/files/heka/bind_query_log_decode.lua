6ee39f0f2e5e16a3e63f6e535da58573cc70e1fd
Nick Chappell <nick@intronic.org>
2015-06-10 23:19:19 -0700

    Add some example grammar parsing to the Lua plugin script.

--- networky_services/bind/files/heka/bind_query_log_decode.lua
+++ networky_services/bind/files/heka/bind_query_log_decode.lua
@@ -4,6 +4,10 @@ BIND DNS query log decoder script for the Heka stream processor:
 
 http://hekad.readthedocs.org/en/latest/
 
+Adapted from: https://github.com/mozilla-services/heka/wiki/How-to-convert-a-PayloadRegex-MultiDecoder-to-a-SandboxDecoder-using-an-LPeg-Grammar
+
+Built with the help of: http://lpeg.trink.com/
+
 Example use:
 
 [bind_query_logs]
@@ -22,41 +26,66 @@ filename = "lua_decoders/bind_query_log_decoder.lua"
 Sample BIND query log message, with the print-category, print-severity and print-time options
 all set to 'yes':
 
-27-May-2015 21:06:49.246 queries: info: client 127.0.0.1#33182 (webserver.company.com): query: webserver.company.com IN A +E (127.0.0.1)
+27-May-2015 21:06:49.246 queries: info: client 10.0.1.70#41242 (webserver.company.com): query: webserver.company.com IN A +E (10.0.1.71)
+
+The things we want out of it are:
+
+* The client IP
+* The name that was queried
+* The domain of the name that was queried
+* The record type (A, MX, PTR, etc.)
+* The address of the interface that BIND used for the reply
 
 --]]
 
 require "table"
 require "string"
+require "lpeg"
+local date_time = require "date_time"
+local ip = require "ip_address"
+l.locale(l)
 
 local syslog   = require "syslog"
 local formats  = read_config("formats")
 --The config for the SandboxDecoder plugin should have the type set to 'bindquerylog'
 local msg_type = read_config("type")
 
-local dt = require "date_time"
+local space = l.space
+local timestamp = l.Cg(date_time.build_strftime_grammar("%Y/%m/%d %H:%M:%S") / date_time.time_to_ns, "Timestamp")
+local remoteaddr = l.P"remoteaddr=" * l.Cg(l.Ct(l.Cg(ip.v4, "value") * l.Cg(l.Cc"ipv4", "representation")), "Remoteaddr")
+local headers = l.Cg(l.P(1)^0, "Headers")
+local bind_query = date * space * timestamp * space * 'queries:' * 'info:' * 'client' * space * client_ip * space * '(' * query * '):' * space * 'query:' * space * query * space * 'IN' * record_type * space '+E' * space * '(' * reply_address * ')' 
+
+--Individual pieces I need to create patterns for:
+--date
+--timestamp
+--client_ip
+--query
+--record_type
+--reply_address
+
+grammar = l.Ct(bind_query)
 
 local msg = {
-
   Type        = msg_type,
   Payload     = nil,
   Pid         = nil,
   Severity    = nil,
-  Fields      = {  Query      = nil,
-                   RecordType = nil,
-                   ClientIP   = nil
-                   }
+  Fields      = {
+    -- If the query was for webserver.company.com, the fields would get filled in as follows:
+    -- webserver
+    Query       = nil, -- webserber
+    QueryDomain = nil, -- company.com
+    RecordType  = nil, -- A, MX, PTR, etc.
+    ClientIP    = nil
+  }
 }
 
 function process_message ()
-  
+
   local query_log_line = read_message("Payload")
-  local fields = grammar:match(log)
   if not fields then return -1 end
-
-
-  --Set the time in the message we're generating and set it to nil in the original
-  --log line:
+  --Set the time in the message we're generating and set it to nil in the original log line:
   msg.Timestamp = fields.time
   fields.time = nil
 
@@ -64,4 +93,5 @@ function process_message ()
   inject_message(msg)
 
   return 0
+
 end
